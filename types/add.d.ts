821fe112a0932f72fb44a85a895a58f0d325a2a3
Harris Miller <harrismillerconsulting@gmail.com>
2022-12-01 20:53:30 -0700

    comment for add.d.ts

--- types/add.d.ts
+++ types/add.d.ts
@@ -1,2 +1,3 @@
+// no need to type for __: Placeholder, because you'll never need to with addition
 export function add(a: number, b: number): number;
 export function add(a: number): (b: number) => number;
