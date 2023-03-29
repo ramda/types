88a00c457e85bcf884d6e7bd5793bb0de6a61d76
harris-miller <harrismillerconsulting@gmail.com>
2023-03-29 17:21:46 -0600

    redo has so @types/ramda test continue to pass

--- types/has.d.ts
+++ types/has.d.ts
@@ -1,7 +1,11 @@
 import { Placeholder } from './util/tools';
 
-// `has` is just `has = (prop, obj) => `Object.prototype.hasOwnProperty.call(obj, prop)`;
-// Go to Definition gives `hasOwnProperty(v: PropertyKey): boolean;`
-export function has(__: Placeholder, obj: unknown): (s: PropertyKey) => boolean;
-export function has(prop: PropertyKey, obj: unknown): boolean;
-export function has(prop: PropertyKey): (obj: unknown) => boolean;
+type ObjectHavingSome<Key extends string> = {
+  [K in Key]: { [P in K]: unknown };
+}[Key];
+
+export function has(__: Placeholder, obj: unknown): <P extends string>(s: P) => obj is ObjectHavingSome<P>;
+export function has<P extends string>(s: P, obj: unknown): obj is ObjectHavingSome<P>;
+export function has(__: Placeholder): <P extends string>(obj: unknown, s: P) => obj is ObjectHavingSome<P>;
+export function has<P extends string>(s: P): (obj: unknown) => obj is ObjectHavingSome<P>;
+
