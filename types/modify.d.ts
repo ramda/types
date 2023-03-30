4263d4f18ffc4cbc6699e15b8098c6eb80f59247
harris-miller <harrismillerconsulting@gmail.com>
2023-03-29 18:34:21 -0600

    fix lint errors

--- types/modify.d.ts
+++ types/modify.d.ts
@@ -1,9 +1,9 @@
 export function modify<T extends object, K extends keyof T, P>(
-    prop: K,
-    fn: (a: T[K]) => P,
-    obj: T,
+  prop: K,
+  fn: (a: T[K]) => P,
+  obj: T,
 ): Omit<T, K> & Record<K, P>;
 export function modify<K extends string, A, P>(
-    prop: K,
-    fn: (a: A) => P,
+  prop: K,
+  fn: (a: A) => P,
 ): <T extends Record<K, A>>(target: T) => Omit<T, K> & Record<K, P>;
