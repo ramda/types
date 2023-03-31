dcd2fddb3e2accf3fb1c1dbabc3c559d5efd4f08
harris-miller <harrismillerconsulting@gmail.com>
2023-03-31 00:32:39 -0600

    update forEach types

--- types/forEach.d.ts
+++ types/forEach.d.ts
@@ -1,2 +1,5 @@
+import { Placeholder } from './util/tools';
+
 export function forEach<T>(fn: (x: T) => void, list: readonly T[]): T[];
+export function forEach<T>(__: Placeholder, list: readonly T[]): (fn: (x: T) => void) => T[];
 export function forEach<T>(fn: (x: T) => void): (list: readonly T[]) => T[];
