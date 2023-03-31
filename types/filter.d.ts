a6ae7b9a207df9a18adaeab8bd6a882404f019b1
harris-miller <harrismillerconsulting@gmail.com>
2023-03-31 00:32:18 -0600

    update filter types

--- types/filter.d.ts
+++ types/filter.d.ts
@@ -1,12 +1,25 @@
-export function filter<A, P extends A>(
-  pred: (val: A) => val is P,
-): {
-  <B extends A>(list: readonly B[]): P[];
-  <B extends A>(dict: Record<string, B>): Record<string, P>;
-};
-export function filter<T>(
-  pred: (value: T) => boolean,
-): <P extends T, C extends readonly P[] | Record<string, P>>(collection: C) => C;
+import { Placeholder } from './util/tools';
+
+// `val as P` makes sense here for type narrowing
+// TODO: however, `P extends T` does not, need to look into that
+// must have a `boolean` option as well for if predicate does not narrow
+// see for details:
+// https://www.typescriptlang.org/play?#code/C4TwDgpgBAysBOBLAdgcwPLwHIFcC2ARhPFALxQDOCKqUAPlMvkfANwCwAUFwMYD2yKlEQU4SNGSgAKAG4BDADYAuWNTSZchYgEoV8hcIqU1tUgD4ooSHwBmUfWVLkA5FXGpnHTv0HBDmlklZRRUxGg1mHT1FQ0ZIknNLcAhbexinFyYteE9eASERADFFChAg-VCTCOzdKAI+PgUIOWQyCwBCdv0vLgATCB4FOXhoGxxkHmBEASgbRAVgYgAeABUAGigABSgIAA9F5F6jFbMpMBHelWDlKBXtNrSDES2NhRFgFRG5XoEFMpWANoAXVqm2BXn6g2Go3Gk2mrTmC2WJzOFyuFVu90S9UazWQr3en2aP2Qf1uwNqgKBPW8+T8byolXc1RYwMkAIADBtnM4NgBGblyXlQABM3IIzmpXDyvig8D5kkRi3gUhEYTQBKo2i8AHodVADVAAHoAfhlQngIsV82VqooAWImuA2q4esNxrNtNl8AAzNakSqiiUQE6XZw3YbTVwgA
 export function filter<T, P extends T>(pred: (val: T) => val is P, list: readonly T[]): P[];
+export function filter<T>(pred: (val: T) => boolean, list: readonly T[]): T[];
 export function filter<T, P extends T>(pred: (val: T) => val is P, dict: Record<string, T>): Record<string, P>;
-export function filter<T, C extends readonly T[] | Record<string, T>>(pred: (value: T) => boolean, collection: C): C;
+export function filter<T>(pred: (val: T) => boolean, dict: Record<string, T>): Record<string, T>;
+
+export function filter<T>(__: Placeholder, list: readonly T[]): <P extends T>(pred: (val: T) => val is P) => P[];
+export function filter<T>(__: Placeholder, list: readonly T[]): (pred: (val: T) => boolean) => T[];
+export function filter<T>(__: Placeholder, dict: Record<string, T>): <P extends T>(pred: (val: T) => val is P) => Record<string, P>;
+export function filter<T>(__: Placeholder, dict: Record<string, T>): (pred: (val: T) => boolean) => Record<string, T>;
+
+export function filter<T, P extends T>(pred: (val: T) => val is P): {
+  (list: readonly T[]): P[];
+  (dict: Record<string, T>): Record<string, P>;
+};
+export function filter<T>(pred: (val: T) => boolean): {
+  (list: readonly T[]): T[];
+  (dict: Record<string, T>): Record<string, T>;
+};
