de4dac23c354dc47cea507677e4f8938d0e17250
harris-miller <harrismillerconsulting@gmail.com>
2023-04-07 20:49:39 -0600

    thats good

--- types/unwind.d.ts
+++ types/unwind.d.ts
@@ -1,5 +1,15 @@
 import { Placeholder } from './util/tools';
 
-export function unwind<K extends PropertyKey, O extends Record<K, any>>(key: K, obj: O): O[K] extends Array<infer T> ? Array<Omit<K, O> & Record<K, T>> : never;
-export function unwind<O extends object>(__: Placeholder, obj: O): <K extends keyof O>(key: K) => O[K] extends Array<infer T> ? Array<Omit<K, O> & Record<K, T>> : never;
-export function unwind<K extends PropertyKey>(key: K): < O extends Record<K, any>>(obj: O) => O[K] extends Array<infer T> ? Array<Omit<K, O> & Record<K, T>> : never;
+// if key does not extend a property whos value an Array, obj is simply wrapped in an array.
+// Otherwise it is an array of that object, but that specific property retyped as the inner type, eg `Array<T>` becomes `T`
+export function unwind<O extends object>(__: Placeholder, obj: O): {
+  <K extends keyof O>(key: K): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
+  <K extends PropertyKey>(key: K): [O];
+};
+export function unwind<K extends keyof O, O>(key: K, obj: O): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
+// if K does not exist on O, [O] is returned
+export function unwind<K extends PropertyKey, O>(key: K, obj: O): [O];
+export function unwind<K extends PropertyKey>(key: K): {
+  <O extends Record<K, any>>(obj: O): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
+  <O>(obj: O): [O];
+};
