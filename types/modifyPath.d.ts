891bd69f6b18501d692b421251e2c5d20467b0b9
harris-miller <harrismillerconsulting@gmail.com>
2023-04-08 14:40:18 -0600

    got that recursive good good

--- types/modifyPath.d.ts
+++ types/modifyPath.d.ts
@@ -1,19 +1,57 @@
-// import { Path } from './util/tools';
+import { Path, DeepModify } from './util/tools';
 
-// TODO: there has to be a better way to do this
-// Specifically, the recursive Omit/Record combos that act as our returns
-export function modifyPath<U, K0 extends keyof U, T>(path: [K0], fn: (value: U[K0]) => T, obj: U): Omit<U, K0> & Record<K0, T>;
+export function modifyPath<U, T, K0 extends keyof U>(path: [K0], fn: (value: U[K0]) => T, obj: U): DeepModify<[K0], U, T>;
 export function modifyPath<
   U,
+  T,
   K0 extends keyof U,
-  K1 extends keyof U[K0],
-  T
->(path: [K0, K1], fn: (value: U[K0][K1]) => T, obj: U): Omit<U, K0> & Record<K0, Omit<U[K0], K1> & Record<K1, T>>;
-
+  K1 extends keyof U[K0]
+>(path: [K0, K1], fn: (value: U[K0][K1]) => T, obj: U): DeepModify<[K0, K1], U, T>;
 export function modifyPath<
   U,
   K0 extends keyof U,
   K1 extends keyof U[K0],
   K2 extends keyof U[K0][K1],
   T
->(path: [K0, K1], fn: (value: U[K0][K1][K2]) => T, obj: U): Omit<U, K0> & Record<K0, Omit<U[K0], K1> & Record<K1, Record<K2, Omit<U[K0][K1], K2> & Record<K2, T>>>>;
+>(path: [K0, K1, K2], fn: (value: U[K0][K1][K2]) => T, obj: U): DeepModify<[K0, K1, K2], U, T>;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  K2 extends keyof U[K0][K1],
+  K3 extends keyof U[K0][K1][K2],
+  T
+>(path: [K0, K1, K2, K3], fn: (value: U[K0][K1][K2][K3]) => T, obj: U): DeepModify<[K0, K1, K2, K3], U, T>;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  K2 extends keyof U[K0][K1],
+  K3 extends keyof U[K0][K1][K2],
+  K4 extends keyof U[K0][K1][K2][K3],
+  T
+>(path: [K0, K1, K2, K3, K4], fn: (value: U[K0][K1][K2][K3][K4]) => T, obj: U): DeepModify<[K0, K1, K2, K3, K4], U, T>;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  K2 extends keyof U[K0][K1],
+  K3 extends keyof U[K0][K1][K2],
+  K4 extends keyof U[K0][K1][K2][K3],
+  K5 extends keyof U[K0][K1][K2][K3][K4],
+  T
+>(path: [K0, K1, K2, K3, K4, K5], fn: (value: U[K0][K1][K2][K3][K4][K5]) => T, obj: U): DeepModify<[K0, K1, K2, K3, K4, K5], U, T>;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  K2 extends keyof U[K0][K1],
+  K3 extends keyof U[K0][K1][K2],
+  K4 extends keyof U[K0][K1][K2][K3],
+  K5 extends keyof U[K0][K1][K2][K3][K4],
+  K6 extends keyof U[K0][K1][K2][K3][K4][K5],
+  T
+>(path: [K0, K1, K2, K3, K4, K5, K6], fn: (value: U[K0][K1][K2][K3][K4][K5][K6]) => T, obj: U): DeepModify<[K0, K1, K2, K3, K4, K5, K6], U, T>;
+// backup type
+// for unknown path, or key-chain larger than 6, allow user to set output type B and input type A
+export function modifyPath<B, A = any>(path: Path, fn: (a: any) => any, obj: A): B;
