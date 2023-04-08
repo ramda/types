57f5a95cc6fcba8f9c547e5cdb31bb64f25a3f10
harris-miller <harrismillerconsulting@gmail.com>
2023-04-07 21:23:53 -0600

    simplier still

--- types/sequence.d.ts
+++ types/sequence.d.ts
@@ -2,4 +2,4 @@ export { Placeholder } from './util/tools';
 
 // Since typescript doesn't support Higher Kinded Types, it's virtually impossible to type sequence
 // Having some sort of type definition is need at least because
-export function sequence<T, F, T>(of: (a: T) => F, traversable: T[]): F[];
+export function sequence<T, Of extends (a: any) => T = (a: any) => T>(of: Of, traversable: any[]): T;
