f16e80975d9d9861d3c201e6d086584207231a3c
harris-miller <harrismillerconsulting@gmail.com>
2023-04-07 22:27:57 -0600

    WIP

--- types/modifyPath.d.ts
+++ types/modifyPath.d.ts
@@ -1,37 +1,19 @@
 // import { Path } from './util/tools';
 
+// TODO: there has to be a better way to do this
+// Specifically, the recursive Omit/Record combos that act as our returns
 export function modifyPath<U, K0 extends keyof U, T>(path: [K0], fn: (value: U[K0]) => T, obj: U): Omit<U, K0> & Record<K0, T>;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  T
+>(path: [K0, K1], fn: (value: U[K0][K1]) => T, obj: U): Omit<U, K0> & Record<K0, Omit<U[K0], K1> & Record<K1, T>>;
 
-// export function modifyPath<S, K0 extends keyof S = keyof S, K1 extends keyof S[K0] = keyof S[K0]>(path: [K0, K1], obj: S): S[K0][K1];
-// export function modifyPath<
-//   S,
-//   K0 extends keyof S = keyof S,
-//   K1 extends keyof S[K0] = keyof S[K0],
-//   K2 extends keyof S[K0][K1] = keyof S[K0][K1]
-// >(path: [K0, K1, K2], obj: S): S[K0][K1][K2];
-// export function modifyPath<
-//   S,
-//   K0 extends keyof S = keyof S,
-//   K1 extends keyof S[K0] = keyof S[K0],
-//   K2 extends keyof S[K0][K1] = keyof S[K0][K1],
-//   K3 extends keyof S[K0][K1][K2] = keyof S[K0][K1][K2]
-// >(path: [K0, K1, K2, K3], obj: S): S[K0][K1][K2][K3];
-// export function modifyPath<
-//   S,
-//   K0 extends keyof S = keyof S,
-//   K1 extends keyof S[K0] = keyof S[K0],
-//   K2 extends keyof S[K0][K1] = keyof S[K0][K1],
-//   K3 extends keyof S[K0][K1][K2] = keyof S[K0][K1][K2],
-//   K4 extends keyof S[K0][K1][K2][K3] = keyof S[K0][K1][K2][K3]
-// >(path: [K0, K1, K2, K3, K4], obj: S): S[K0][K1][K2][K3][K4];
-// export function modifyPath<
-//   S,
-//   K0 extends keyof S = keyof S,
-//   K1 extends keyof S[K0] = keyof S[K0],
-//   K2 extends keyof S[K0][K1] = keyof S[K0][K1],
-//   K3 extends keyof S[K0][K1][K2] = keyof S[K0][K1][K2],
-//   K4 extends keyof S[K0][K1][K2][K3] = keyof S[K0][K1][K2][K3],
-//   K5 extends keyof S[K0][K1][K2][K3][K4] = keyof S[K0][K1][K2][K3][K4]
-// >(path: [K0, K1, K2, K3, K4, K5], obj: S): S[K0][K1][K2][K3][K4][K5];
-// export function modifyPath<T>(path: Path, obj: any): T | undefined;
-// export function modifyPath<T>(path: Path): (obj: any) => T | undefined;
+export function modifyPath<
+  U,
+  K0 extends keyof U,
+  K1 extends keyof U[K0],
+  K2 extends keyof U[K0][K1],
+  T
+>(path: [K0, K1], fn: (value: U[K0][K1][K2]) => T, obj: U): Omit<U, K0> & Record<K0, Omit<U[K0], K1> & Record<K1, Record<K2, Omit<U[K0][K1], K2> & Record<K2, T>>>>;
