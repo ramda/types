37414e64de11529625740b25fe1e1462bf6948bc
harris-miller <harrismillerconsulting@gmail.com>
2023-04-10 18:57:42 -0600

    Fix: isNotNil back build

--- types/isNotNil.d.ts
+++ types/isNotNil.d.ts
@@ -1,5 +1,3 @@
-import { U } from 'ts-toolbelt';
+import * as _ from 'ts-toolbelt';
 
-export function isNotNil<T>(
-  value: T
-): value is U.NonNullable<T>;
+export function isNotNil<T>(value: T): value is _.U.NonNullable<T>;
