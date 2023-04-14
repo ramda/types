576b4e1e4430ac8c20a52af29745d01c32204ab3
MoyuScript <moyuscript@gmail.com>
2023-04-14 14:17:50 +0800

    Update allPass.ts

--- test/allPass.ts
+++ test/allPass.ts
@@ -1,11 +1,11 @@
 import { expectType, expectError } from 'tsd';
 import { allPass, propEq } from '../es';
 
-const isOld = propEq('age', 212);
-const isAllergicToGarlic = propEq('garlic_allergy', true);
-const isAllergicToSun = propEq('sun_allergy', true);
-const isFast = propEq('fast', null);
-const isAfraid = propEq('fear', undefined);
+const isOld = propEq(212, 'age');
+const isAllergicToGarlic = propEq(true, 'garlic_allergy');
+const isAllergicToSun = propEq(true, 'sun_allergy');
+const isFast = propEq(null, 'fast');
+const isAfraid = propEq(undefined, 'fear');
 
 const isVampire = allPass([
   isOld,
