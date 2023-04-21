4a5e7c51a25c02a5559d6865c11158ec0277918f
harris-miller <harrismillerconsulting@gmail.com>
2023-04-21 12:40:13 -0600

    better tests

--- types/mergeLeft.d.ts
+++ types/mergeLeft.d.ts
@@ -1,5 +1,5 @@
-import * as _ from 'ts-toolbelt';
+import { MergeObjects } from './util/tools';
 
-// Note: ramda `mergeLeft` uses `Object.assign` in code, so we need to use `O.Assign` here, and not `O.Merge`
-export function mergeLeft<L extends object, R extends object>(l: L, r: R): _.O.Assign<R, [L], 'flat'>;
-export function mergeLeft<L extends object>(l: L): <R extends object>(r: R) => _.O.Assign<R, [L], 'flat'>;
+// Note: ramda `mergeLeft` is just `mergeRight` with the operands flipped
+export function mergeLeft<L extends object, R extends object>(l: L, r: R): MergeObjects<R, L>;
+export function mergeLeft<L extends object>(l: L): <R extends object>(r: R) => MergeObjects<R, L>;
