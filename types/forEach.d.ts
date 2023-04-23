f2e5cd9fd22d2cdaf0b18e773a5f33044c929f01
harris-miller <harrismillerconsulting@gmail.com>
2023-04-23 04:24:13 -0600

    forEach

--- types/forEach.d.ts
+++ types/forEach.d.ts
@@ -1,5 +1,10 @@
 import { Placeholder } from './util/tools';
 
-export function forEach<T>(fn: (x: T) => void, list: readonly T[]): T[];
-export function forEach<T>(__: Placeholder, list: readonly T[]): (fn: (x: T) => void) => T[];
-export function forEach<T>(fn: (x: T) => void): (list: readonly T[]) => T[];
+export function forEach<T>(fn: (x: T) => void, list: T[]): T[];
+export function forEach<T>(fn: (x: T) => void, list: readonly T[]): readonly T[];
+export function forEach<T>(__: Placeholder, list: T[]): (fn: (x: T) => void) => T[];
+export function forEach<T>(__: Placeholder, list: readonly T[]): (fn: (x: T) => void) => readonly T[];
+export function forEach<T>(fn: (x: T) => void): {
+  (list: T[]): T[];
+  (list: readonly T[]): readonly T[];
+};
