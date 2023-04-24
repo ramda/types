e097d26a793179bce96e9f83393b418e1debc749
harris-miller <harrismillerconsulting@gmail.com>
2023-04-23 18:50:30 -0600

    pull over changes

--- types/forEach.d.ts
+++ types/forEach.d.ts
@@ -1,2 +1,5 @@
-export function forEach<T>(fn: (x: T) => void, list: readonly T[]): T[];
-export function forEach<T>(fn: (x: T) => void): (list: readonly T[]) => T[];
+import { Placeholder } from './util/tools';
+
+export function forEach<T, U extends readonly T[] = readonly T[]>(fn: (x: T) => void, list: U): U;
+export function forEach<U extends readonly any[] = readonly any[]>(__: Placeholder, list: U): (fn: (x: U extends readonly (infer T)[] ? T : never) => void) => U;
+export function forEach<T>(fn: (x: T) => void): <U extends readonly T[]>(list: U) => U;
