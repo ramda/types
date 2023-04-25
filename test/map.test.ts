b290b1cce45b304b8d8449fcb1951f863c1563e5
harris-miller <harrismillerconsulting@gmail.com>
2023-04-24 20:47:53 -0600

    pull over changes from addIndex branch

---
+++ test/map.test.ts
@@ -0,0 +1,37 @@
+import { expectType, expectAssignable } from 'tsd';
+import { __, FunctorMap, map, toString } from '../es';
+
+const arr: number[] = [];
+const arrRO: readonly number[] = [];
+
+// array
+expectType<string[]>(map(toString, arr));
+expectType<string[]>(map(toString, arrRO));
+
+expectType<string[]>(map(__, arr)(toString));
+expectType<string[]>(map(__, arrRO)(toString));
+
+expectType<string[]>(map(toString)(arr));
+expectType<string[]>(map(toString)(arrRO));
+
+
+// object
+expectType<Record<string, string>>(map(toString, {} as Record<string, number>));
+expectType<Record<string, string>>(map(__, {} as Record<string, number>)(toString));
+expectType<Record<string, string>>(map(toString)({} as Record<string, number>));
+
+
+// functor
+type TestFunctorMap<A> = {
+  map: <B>(fn: (a: A) => B) => TestFunctorMap<B>;
+};
+
+expectType<FunctorMap<string>>(map(toString, {} as TestFunctorMap<number>));
+expectAssignable<TestFunctorMap<string>>(map(toString, {} as TestFunctorMap<number>));
+
+type TestFunctorFantasyLand<A> = {
+  ['fantasy-land/map']: <B>(fn: (a: A) => B) => TestFunctorFantasyLand<B>;
+};
+
+expectType<TestFunctorFantasyLand<string>>(map(__, {} as TestFunctorFantasyLand<number>)(toString));
+expectType<TestFunctorFantasyLand<string>>(map(toString)({} as TestFunctorFantasyLand<number>));
--- types/map.d.ts
+++ types/map.d.ts
@@ -1,9 +1,18 @@
-import { ValueOfUnion, Functor } from './util/tools';
+import { FunctorMap, FunctorFantasyLand, Placeholder, ValueOfUnion } from './util/tools';
 
 export function map<T, U>(fn: (x: T) => U, list: readonly T[]): U[];
-export function map<T, U>(fn: (x: T) => U): (list: readonly T[]) => U[];
-export function map<T, U>(fn: (x: T[keyof T & keyof U] | ValueOfUnion<T>) => U[keyof T & keyof U], list: T): U;
-export function map<T, U>(fn: (x: T[keyof T & keyof U] | ValueOfUnion<T>) => U[keyof T & keyof U]): (list: T) => U;
-// used in functors
-export function map<T, U>(fn: (x: T) => U, obj: Functor<T>): Functor<U>;
-export function map<T, U>(fn: (x: T) => U): (obj: Functor<T>) => Functor<U>;
+export function map<T, U>(fn: (x: T) => U, obj: FunctorMap<T>): FunctorMap<U>;
+export function map<T, U>(fn: (x: T) => U, obj: FunctorFantasyLand<T>): FunctorFantasyLand<U>;
+export function map<O extends object, U>(fn: (x: ValueOfUnion<O>) => U, dict: O): Record<keyof O, U>;
+
+export function map<T>(__: Placeholder, list: readonly T[]): <U>(fn: (x: T) => U) => U[];
+export function map<A>(__: Placeholder, obj: FunctorMap<A>): <B>(fn: (a: A) => B) => FunctorMap<B>;
+export function map<A>(__: Placeholder, obj: FunctorFantasyLand<A>): <B>(fn: (a: A) => B) => FunctorFantasyLand<B>;
+export function map<O extends object>(__: Placeholder, dict: O): <U>(fn: (x: ValueOfUnion<O>) => U) => Record<keyof O, U>;
+
+export function map<T, U>(fn: (x: T) => U): {
+  (list: readonly T[]): U[];
+  (obj: FunctorMap<T>): FunctorMap<U>;
+  (obj: FunctorFantasyLand<T>): FunctorFantasyLand<U>;
+  <O extends Record<string, T>>(dict: O): Record<keyof O, U>;
+};
