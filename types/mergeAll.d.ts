b07700524f45bff65e9a596255c527108b954779
harris-miller <harrismillerconsulting@gmail.com>
2023-04-25 09:02:41 -0600

    overload for mergeAll

--- types/mergeAll.d.ts
+++ types/mergeAll.d.ts
@@ -1,3 +1,4 @@
 import * as _ from 'ts-toolbelt';
 
 export function mergeAll<O extends object, Os extends readonly object[]>(list: [O, ...Os]): _.O.Assign<O, Os>;
+export function mergeAll<O>(list: readonly O[]): O;
