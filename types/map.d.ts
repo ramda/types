a8843f91b069f6b83643fdb3fb027e8593b3c352
harris-miller <harrismillerconsulting@gmail.com>
2023-04-25 02:46:40 -0600

    holy fuck that worked

--- types/map.d.ts
+++ types/map.d.ts
@@ -10,9 +10,24 @@ export function map<A>(__: Placeholder, obj: FunctorMap<A>): <B>(fn: (a: A) => B
 export function map<A>(__: Placeholder, obj: FunctorFantasyLand<A>): <B>(fn: (a: A) => B) => FunctorFantasyLand<B>;
 export function map<O extends object>(__: Placeholder, dict: O): <U>(fn: (x: ValueOfUnion<O>) => U) => Record<keyof O, U>;
 
-export function map<T, U>(fn: (x: T) => U): {
+function map<H extends 'list' | 'obj' | 'functor' | 'fantasyland' = 'list', T = any, U = any>(fn: (x: T) => U): {
+  // the first 4 overloads work for `map(fn)(a)` or `const mapF = map(fn); mapF(a)` usages
+  // in these circumstances `a` will auto pic the correct overload
   (list: readonly T[]): U[];
   (obj: FunctorMap<T>): FunctorMap<U>;
   (obj: FunctorFantasyLand<T>): FunctorFantasyLand<U>;
-  <O extends Record<string, T>>(dict: O): Record<keyof O, U>;
+  <O extends Record<PropertyKey, T>>(dict: O): Record<keyof O, U>;
+  // that doesn't when passing `map` as a prop to another function like `pipe` or `compose`
+  // this fallback takes over in those cases, and lets you use the generic helper `H` to set what the expected param type needs to be
+  <O extends Record<PropertyKey, T>>(functor: {
+    'list': readonly T[];
+    'functor': FunctorMap<T>;
+    'fantasyland': FunctorFantasyLand<T>;
+    'obj': O;
+  }[H extends infer H_ ? H_ : never]): {
+    'list': U[];
+    'obj': Record<keyof O, U>;
+    'functor': FunctorMap<U>;
+    'fantasyland': FunctorFantasyLand<U>;
+  }[H extends infer H_ ? H_ : never];
 };
