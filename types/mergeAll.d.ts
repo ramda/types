790c81eb9956407b83e52b66cebcae9a3b5825b6
harris-miller <harrismillerconsulting@gmail.com>
2023-04-25 09:26:06 -0600

    wait, return T, not T[]

--- types/mergeAll.d.ts
+++ types/mergeAll.d.ts
@@ -1,10 +1,8 @@
 import * as _ from 'ts-toolbelt';
 
-// for when passing in an object literal of different objects, eg `mergeAll([obj1: O1, obj2: O2, obj3: O3])`
-// you get back essentially a cleaner version of `O1 & O2 & O3`
-export function mergeAll<O extends object, Os extends readonly object[]>(list: [O, ...Os]): _.O.Assign<O, Os>;
-// for when passing in an `O[]` where all the objects are the same shape `mergeAll([obj1, obj2, obj3]: O[])
-// this just returns the original type back
-export function mergeAll<O>(list: O[]): O[];
-// if O[] is readonly only, give back readonly
-export function mergeAll<O>(list: readonly O[]): readonly O[];
+// for when passing in an object literal of different objects, eg `mergeAll([obj1: T1, obj2: T2, obj3: T3])`
+// you get back essentially a cleaner version of `T1 & T2 & T3`
+export function mergeAll<T extends object, Ts extends readonly object[]>(list: [T, ...Ts]): _.O.Assign<T, Ts>;
+// for when passing in an `T[]` where all the objects are the same shape `mergeAll([obj1, obj2, obj3]: T[])
+// this just returns T
+export function mergeAll<T>(list: readonly T[]): T;
