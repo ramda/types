b6b4db71c61c6e695acb8244bb8234c66fbdb869
Harris Miller <harrismillerconsulting@gmail.com>
2023-04-25 17:02:34 -0600

    Fix: mergeAll (#23)

--- test/mergeAll.test.ts
+++ test/mergeAll.test.ts
@@ -9,7 +9,15 @@ import { mergeAll } from '../es';
 const foo = { foo: 'foo' };
 const bar = { bar: 'bar' };
 const foo2 = { foo: 2 };
+const fooA = [foo, foo];
 
 expectType<{ foo: string; }>(mergeAll([foo]));
+expectType<{ foo: string; }>(mergeAll([foo, foo]));
+expectType<{ foo: string; }>(mergeAll([foo, { ...foo }]));
 expectType<{ foo: string; bar: string; }>(mergeAll([foo, bar]));
 expectType<{ foo: number; }>(mergeAll([foo, foo2]));
+expectType<{ foo: string }>(mergeAll(fooA));
+
+const list: Array<{x: number}> = [{x: 1}, {x: 2}];
+expectType<{x: number}>(mergeAll(list));
+
