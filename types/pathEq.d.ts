b68a3ce996ff406f4c4cc061e754159dd986c7c8
harris-miller <harrismillerconsulting@gmail.com>
2023-04-29 17:13:28 -0600

    fix argument order for 0.29.0 release

--- types/pathEq.d.ts
+++ types/pathEq.d.ts
@@ -1,6 +1,8 @@
-import * as _ from 'ts-toolbelt';
 import { Path } from './util/tools';
 
-export function pathEq(path: Path, val: any, obj: any): boolean;
-export function pathEq(path: Path, val: any): (obj: any) => boolean;
-export function pathEq(path: Path): _.F.Curry<(a: any, b: any) => boolean>;
+export function pathEq(val: any, path: Path, obj: any): boolean;
+export function pathEq(val: any, path: Path): (obj: any) => boolean;
+export function pathEq(val: any): {
+  (path: Path): (obj: any) => boolean;
+  (path: Path, obj: any): boolean;
+};
