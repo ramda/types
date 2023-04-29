23dd3f39d760b3d25eddf1dbf3f85558afd88d17
harris-miller <harrismillerconsulting@gmail.com>
2023-04-29 16:48:18 -0600

    re-order overloads

--- types/compose.d.ts
+++ types/compose.d.ts
@@ -1,35 +1,26 @@
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
-  ...func: [
-        fnLast: (a: any) => TResult,
-        ...func: Array<(a: any) => any>,
-        f7: (a: R6) => R7,
-        f6: (a: R5) => R6,
-        f5: (a: R4) => R5,
-        f4: (a: R3) => R4,
-        f3: (a: R2) => R3,
-        f2: (a: R1) => R2,
-        f1: (...args: TArgs) => R1
-  ]
-): (...args: TArgs) => TResult;
-// fallback overload if number of composed functions greater than 7
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
-  f7: (a: R6) => R7,
-  f6: (a: R5) => R6,
+export function compose<TArgs extends any[], R1>(f1: (...args: TArgs) => R1): (...args: TArgs) => R1;
+export function compose<TArgs extends any[], R1, R2>(
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R2;
+export function compose<TArgs extends any[], R1, R2, R3>(
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R3;
+export function compose<TArgs extends any[], R1, R2, R3, R4>(
+  f4: (a: R3) => R4,
+  f3: (a: R2) => R3,
+  f2: (a: R1) => R2,
+  f1: (...args: TArgs) => R1,
+): (...args: TArgs) => R4;
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5>(
   f5: (a: R4) => R5,
   f4: (a: R3) => R4,
   f3: (a: R2) => R3,
   f2: (a: R1) => R2,
   f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R7;
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
-  f7: (a: R6) => R7,
-  f6: (a: R5) => R6,
-  f5: (a: R4) => R5,
-  f4: (a: R3) => R4,
-  f3: (a: R2) => R3,
-  f2: (a: R1) => R2,
-  f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R7;
+): (...args: TArgs) => R5;
 export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
   f6: (a: R5) => R6,
   f5: (a: R4) => R5,
@@ -38,26 +29,21 @@ export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
   f2: (a: R1) => R2,
   f1: (...args: TArgs) => R1,
 ): (...args: TArgs) => R6;
-export function compose<TArgs extends any[], R1, R2, R3, R4, R5>(
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
+  f7: (a: R6) => R7,
+  f6: (a: R5) => R6,
   f5: (a: R4) => R5,
   f4: (a: R3) => R4,
   f3: (a: R2) => R3,
   f2: (a: R1) => R2,
   f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R5;
-export function compose<TArgs extends any[], R1, R2, R3, R4>(
-  f4: (a: R3) => R4,
-  f3: (a: R2) => R3,
-  f2: (a: R1) => R2,
-  f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R4;
-export function compose<TArgs extends any[], R1, R2, R3>(
-  f3: (a: R2) => R3,
-  f2: (a: R1) => R2,
-  f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R3;
-export function compose<TArgs extends any[], R1, R2>(
-  f2: (a: R1) => R2,
-  f1: (...args: TArgs) => R1,
-): (...args: TArgs) => R2;
-export function compose<TArgs extends any[], R1>(f1: (...args: TArgs) => R1): (...args: TArgs) => R1;
+): (...args: TArgs) => R7;
+// fallback overload that covers anything greater than 7, but doesn't type check between functions
+export function compose<TArgs extends any[], R1, TResult>(
+  ...func: [
+    fnLast: (a: any) => TResult,
+    ...func: Array<(a: any) => any>,
+    f1: (...args: TArgs) => R1
+  ]
+): (...args: TArgs) => TResult;
+
--- types/pipe.d.ts
+++ types/pipe.d.ts
@@ -1,17 +1,34 @@
-export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
-  ...funcs: [
-        f1: (...args: TArgs) => R1,
-        f2: (a: R1) => R2,
-        f3: (a: R2) => R3,
-        f4: (a: R3) => R4,
-        f5: (a: R4) => R5,
-        f6: (a: R5) => R6,
-        f7: (a: R6) => R7,
-        ...func: Array<(a: any) => any>,
-        fnLast: (a: any) => TResult
-  ]
-): (...args: TArgs) => TResult;
-// fallback overload if number of piped functions greater than 7
+export function pipe<TArgs extends any[], R1>(f1: (...args: TArgs) => R1): (...args: TArgs) => R1;
+export function pipe<TArgs extends any[], R1, R2>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+): (...args: TArgs) => R2;
+export function pipe<TArgs extends any[], R1, R2, R3>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+): (...args: TArgs) => R3;
+export function pipe<TArgs extends any[], R1, R2, R3, R4>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+): (...args: TArgs) => R4;
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+): (...args: TArgs) => R5;
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
+  f1: (...args: TArgs) => R1,
+  f2: (a: R1) => R2,
+  f3: (a: R2) => R3,
+  f4: (a: R3) => R4,
+  f5: (a: R4) => R5,
+  f6: (a: R5) => R6,
+): (...args: TArgs) => R6;
 export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f1: (...args: TArgs) => R1,
   f2: (a: R1) => R2,
@@ -21,34 +38,11 @@ export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f6: (a: R5) => R6,
   f7: (a: R6) => R7,
 ): (...args: TArgs) => R7;
-export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6>(
-  f1: (...args: TArgs) => R1,
-  f2: (a: R1) => R2,
-  f3: (a: R2) => R3,
-  f4: (a: R3) => R4,
-  f5: (a: R4) => R5,
-  f6: (a: R5) => R6,
-): (...args: TArgs) => R6;
-export function pipe<TArgs extends any[], R1, R2, R3, R4, R5>(
-  f1: (...args: TArgs) => R1,
-  f2: (a: R1) => R2,
-  f3: (a: R2) => R3,
-  f4: (a: R3) => R4,
-  f5: (a: R4) => R5,
-): (...args: TArgs) => R5;
-export function pipe<TArgs extends any[], R1, R2, R3, R4>(
-  f1: (...args: TArgs) => R1,
-  f2: (a: R1) => R2,
-  f3: (a: R2) => R3,
-  f4: (a: R3) => R4,
-): (...args: TArgs) => R4;
-export function pipe<TArgs extends any[], R1, R2, R3>(
-  f1: (...args: TArgs) => R1,
-  f2: (a: R1) => R2,
-  f3: (a: R2) => R3,
-): (...args: TArgs) => R3;
-export function pipe<TArgs extends any[], R1, R2>(
-  f1: (...args: TArgs) => R1,
-  f2: (a: R1) => R2,
-): (...args: TArgs) => R2;
-export function pipe<TArgs extends any[], R1>(f1: (...args: TArgs) => R1): (...args: TArgs) => R1;
+// fallback overload that covers anything greater than 7, but doesn't type check between functions
+export function pipe<TArgs extends any[], R1, TResult>(
+  ...funcs: [
+    f1: (...args: TArgs) => R1,
+    ...func: Array<(a: any) => any>,
+    fnLast: (a: any) => TResult
+  ]
+): (...args: TArgs) => TResult;
