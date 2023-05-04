5a72b01b063d837cfb2f7a3d210c699a61a9d5e6
Mike Lambert <lax4mike@gmail.com>
2023-05-04 11:11:14 -0400

    Using Exclude , adding tests

--- test/defaultTo.test.ts
+++ test/defaultTo.test.ts
@@ -1,13 +1,13 @@
 import { expectType } from 'tsd';
 import { defaultTo } from '../es';
 
-expectType<string>(defaultTo('no value', undefined));
-expectType<string>(defaultTo('no value', null));
-expectType<string>(defaultTo('no value')(undefined));
-expectType<string>(defaultTo('no value')(null));
+expectType<string>(defaultTo('default value', undefined));
+expectType<string>(defaultTo('default value', null));
+expectType<string>(defaultTo('default value')(undefined));
+expectType<string>(defaultTo('default value')(null));
 
-expectType<string>(defaultTo('no value', 'string value'));
-expectType<string>(defaultTo('no value')('string value'));
+expectType<string>(defaultTo('default value', 'string value'));
+expectType<string>(defaultTo('default value')('string value'));
 
 expectType<number>(defaultTo(0, undefined));
 expectType<number>(defaultTo(0, null));
@@ -22,16 +22,16 @@ expectType<string>(defaultTo(0)('two'));
 expectType<number>(defaultTo('zero', 2));
 expectType<number>(defaultTo('zero')(2));
 
-
-const stringOrUndefined = (() => Math.random() < 0.5 ? Math.random().toString(32) : undefined)();
+const stringOrUndefined = Math.random() < 0.5 ? Math.random().toString(32) : undefined;
 expectType<number | string>(defaultTo(0, stringOrUndefined));
-const stringOrNull = (() => Math.random() < 0.5 ? Math.random().toString(32) : undefined)();
+expectType<number | string>(defaultTo(0)(stringOrUndefined));
+const stringOrNull = Math.random() < 0.5 ? Math.random().toString(32) : null;
 expectType<number | string>(defaultTo(0, stringOrNull));
-
+expectType<number | string>(defaultTo(0)(stringOrNull));
 
 const numberOrUndefined = Math.random() < 0.5 ? Math.random() : undefined;
-expectType<string | number>(defaultTo('no value', numberOrUndefined));
+expectType<string | number>(defaultTo('default value', numberOrUndefined));
+expectType<string | number>(defaultTo('default value')(numberOrUndefined));
 const numberOrNull = Math.random() < 0.5 ? Math.random() : null;
-expectType<string | number>(defaultTo('no value', numberOrNull));
-
-
+expectType<string | number>(defaultTo('default value', numberOrNull));
+expectType<string | number>(defaultTo('default value')(numberOrNull));
--- types/defaultTo.d.ts
+++ types/defaultTo.d.ts
@@ -1,15 +1,12 @@
 
-export function defaultTo<Default, Value>(a: Default, b: Value): (Value extends (null | undefined) ? Default | NonNullable<Value> : Value);
-export function defaultTo<Default>(a: Default): <Value>(b: Value) => (Value extends (null | undefined) ? Default | NonNullable<Value> : Value);
+export function defaultTo<Default, Value>(a: Default, b: Value): (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
+export function defaultTo<Default>(a: Default): <Value>(b: Value) => (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
 
 
-type DefaultTo<Default, Value> = (Value extends (null | undefined) ? Default | NonNullable<Value> : Value);
+type DefaultTo<Default, Value> = (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
 
 type ShouldBeString1 = DefaultTo<string, undefined>;
 type ShouldBeString2 = DefaultTo<string, null>;
 type ShouldBeNumber = DefaultTo<string, number>;
 type ShouldBeStringOrNumber1 = DefaultTo<string, number | undefined>;
 type ShouldBeStringOrNumber2 = DefaultTo<string, number | null>;
-
-
-
