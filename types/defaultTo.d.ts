169ac10e8f4ca91d25e0f8cff8f491ec81d970a1
Mike Lambert <lax4mike@gmail.com>
2023-05-04 22:45:13 -0400

    Default to Fallback

--- types/defaultTo.d.ts
+++ types/defaultTo.d.ts
@@ -1,12 +1,13 @@
 
-export function defaultTo<Default, Value>(a: Default, b: Value): (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
-export function defaultTo<Default>(a: Default): <Value>(b: Value) => (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
+export function defaultTo<Fallback, Value>(a: Fallback, b: Value): (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
+export function defaultTo<Fallback>(a: Fallback): <Value>(b: Value) => (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
 
 
-type DefaultTo<Default, Value> = (Value extends (null | undefined) ? Default | Exclude<Value, null | undefined> : Value);
+type DefaultTo<Fallback, Value> = (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
 
 type ShouldBeString1 = DefaultTo<string, undefined>;
 type ShouldBeString2 = DefaultTo<string, null>;
+type ShouldBeString3 = DefaultTo<number, string>;
 type ShouldBeNumber = DefaultTo<string, number>;
 type ShouldBeStringOrNumber1 = DefaultTo<string, number | undefined>;
 type ShouldBeStringOrNumber2 = DefaultTo<string, number | null>;
