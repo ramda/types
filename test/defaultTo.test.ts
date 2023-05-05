475080f1e99404d1b395c40e916c24a11676112a
Mike Lambert <lax4mike@gmail.com>
2023-05-05 09:37:57 -0400

    Fixing defaultTo tests

--- test/defaultTo.test.ts
+++ test/defaultTo.test.ts
@@ -1,37 +1,37 @@
 import { expectType } from 'tsd';
 import { defaultTo } from '../es';
 
-expectType<string>(defaultTo('default value', undefined));
-expectType<string>(defaultTo('default value', null));
-expectType<string>(defaultTo('default value')(undefined));
-expectType<string>(defaultTo('default value')(null));
+expectType<'fallback'>(defaultTo('fallback', undefined));
+expectType<'fallback'>(defaultTo('fallback', null));
+expectType<string>(defaultTo('fallback')(undefined));
+expectType<string>(defaultTo('fallback')(null));
 
-expectType<string>(defaultTo('default value', 'string value'));
-expectType<string>(defaultTo('default value')('string value'));
+expectType<'string value'>(defaultTo('fallback', 'string value'));
+expectType<'string value'>(defaultTo('fallback')('string value'));
 
-expectType<number>(defaultTo(0, undefined));
-expectType<number>(defaultTo(0, null));
+expectType<0>(defaultTo(0, undefined));
+expectType<0>(defaultTo(0, null));
 expectType<number>(defaultTo(0)(undefined));
 expectType<number>(defaultTo(0)(null));
 
-expectType<number>(defaultTo(0, 2));
-expectType<number>(defaultTo(0)(2));
+expectType<2>(defaultTo(0, 2));
+expectType<2>(defaultTo(0)(2));
 
-expectType<string>(defaultTo(0, 'two'));
-expectType<string>(defaultTo(0)('two'));
-expectType<number>(defaultTo('zero', 2));
-expectType<number>(defaultTo('zero')(2));
+expectType<'two'>(defaultTo(0, 'two'));
+expectType<'two'>(defaultTo(0)('two'));
+expectType<2>(defaultTo('zero', 2));
+expectType<2>(defaultTo('zero')(2));
 
 const stringOrUndefined = Math.random() < 0.5 ? Math.random().toString(32) : undefined;
-expectType<number | string>(defaultTo(0, stringOrUndefined));
+expectType<0 | string>(defaultTo(0, stringOrUndefined));
 expectType<number | string>(defaultTo(0)(stringOrUndefined));
 const stringOrNull = Math.random() < 0.5 ? Math.random().toString(32) : null;
-expectType<number | string>(defaultTo(0, stringOrNull));
+expectType<0 | string>(defaultTo(0, stringOrNull));
 expectType<number | string>(defaultTo(0)(stringOrNull));
 
 const numberOrUndefined = Math.random() < 0.5 ? Math.random() : undefined;
-expectType<string | number>(defaultTo('default value', numberOrUndefined));
-expectType<string | number>(defaultTo('default value')(numberOrUndefined));
+expectType<'fallback' | number>(defaultTo('fallback', numberOrUndefined));
+expectType<string | number>(defaultTo('fallback')(numberOrUndefined));
 const numberOrNull = Math.random() < 0.5 ? Math.random() : null;
-expectType<string | number>(defaultTo('default value', numberOrNull));
-expectType<string | number>(defaultTo('default value')(numberOrNull));
+expectType<'fallback' | number>(defaultTo('fallback', numberOrNull));
+expectType<string | number>(defaultTo('fallback')(numberOrNull));
