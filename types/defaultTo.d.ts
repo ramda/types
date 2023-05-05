4dc19b3a345600813348d07d8abcfcc1619dffad
Mike Lambert <lax4mike@gmail.com>
2023-05-05 09:47:06 -0400

    Adding DefaultTo to tools.d.ts

--- types/defaultTo.d.ts
+++ types/defaultTo.d.ts
@@ -1,13 +1,4 @@
+import { DefaultTo } from './util/tools';
 
-export function defaultTo<Fallback, Value>(a: Fallback, b: Value): (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
-export function defaultTo<Fallback>(a: Fallback): <Value>(b: Value) => (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
-
-
-type DefaultTo<Fallback, Value> = (Value extends (null | undefined) ? Fallback | Exclude<Value, null | undefined> : Value);
-
-type ShouldBeString1 = DefaultTo<string, undefined>;
-type ShouldBeString2 = DefaultTo<string, null>;
-type ShouldBeString3 = DefaultTo<number, string>;
-type ShouldBeNumber = DefaultTo<string, number>;
-type ShouldBeStringOrNumber1 = DefaultTo<string, number | undefined>;
-type ShouldBeStringOrNumber2 = DefaultTo<string, number | null>;
+export function defaultTo<Fallback, Value>(a: Fallback, b: Value): DefaultTo<Fallback, Value>;
+export function defaultTo<Fallback>(a: Fallback): <Value>(b: Value) => DefaultTo<Fallback, Value>;
