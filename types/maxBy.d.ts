d54565b5148d2c021e448bcfcbeeb04065261e74
harris-miller <harrismillerconsulting@gmail.com>
2023-05-05 18:08:12 -0600

    minBy and maxBy

--- types/maxBy.d.ts
+++ types/maxBy.d.ts
@@ -1,6 +1,37 @@
-import * as _ from 'ts-toolbelt';
-import { Ord } from './util/tools';
+import { Placeholder, Ord } from './util/tools';
 
-export function maxBy<T>(keyFn: (a: T) => Ord): _.F.Curry<(a: T, b: T) => T>;
-export function maxBy<T>(keyFn: (a: T) => Ord, a: T): (b: T) => T;
-export function maxBy<T>(keyFn: (a: T) => Ord, a: T, b: T): T;
+// maxBy(fn)
+export function maxBy<T>(fn: (a: T) => Ord): {
+  // maxBy(fn)(a)(b)
+  (a: T): (b: T) => T;
+  // maxBy(fn)(_, b)
+  (__: Placeholder, b: T): (a: T) => T;
+  // maxBy(fn)(a, b)
+  (a: T, b: T): T;
+};
+// maxBy(__, a)
+export function maxBy<T>(__: Placeholder, a: T): {
+  // maxBy(__, a)(fn)(b)
+  (fn: (a: T) => Ord): (b: T) => T;
+  // maxBy(__, a)(__, b)(fn)
+  (__: Placeholder, b: T): (fn: (a: T) => Ord) => T;
+  // maxBy(__, a)(fn, b)
+  (fn: (a: T) => Ord, b: T): T;
+};
+// maxBy(fn, a)(b)
+export function maxBy<T>(fn: (a: T) => Ord, a: T): (b: T) => T;
+// maxBy(__, __, b)
+export function maxBy<T>(__: Placeholder, __2: Placeholder, b: T): {
+  // maxBy(__, __, b)(fn)(a)
+  (fn: (a: T) => Ord): (a: T) => T;
+  // maxBy(__, __, b)(__, a)(fn)
+  (__: Placeholder, a: T): (fn: (a: T) => Ord) => T;
+  // maxBy(__, __, b)(fn, a)
+  (fn: (a: T) => Ord, a: T): T;
+};
+// maxBy(__, a, b)(fn)
+export function maxBy<T>(__: Placeholder, a: T, b: T): (fn: (a: T) => Ord) => T;
+// maxBy(fn, __, b)
+export function maxBy<T>(fn: (a: T) => Ord, __: Placeholder, b: T): (a: T) => T;
+// maxBy(fn, a, b)
+export function maxBy<T>(fn: (a: T) => Ord, a: T, b: T): T;
--- types/minBy.d.ts
+++ types/minBy.d.ts
@@ -1,6 +1,37 @@
-import * as _ from 'ts-toolbelt';
-import { Ord } from './util/tools';
+import { Placeholder, Ord } from './util/tools';
 
-export function minBy<T>(keyFn: (a: T) => Ord): _.F.Curry<(a: T, b: T) => T>;
-export function minBy<T>(keyFn: (a: T) => Ord, a: T): (b: T) => T;
-export function minBy<T>(keyFn: (a: T) => Ord, a: T, b: T): T;
+// minBy(fn)
+export function minBy<T>(fn: (a: T) => Ord): {
+  // minBy(fn)(a)(b)
+  (a: T): (b: T) => T;
+  // minBy(fn)(_, b)
+  (__: Placeholder, b: T): (a: T) => T;
+  // minBy(fn)(a, b)
+  (a: T, b: T): T;
+};
+// minBy(__, a)
+export function minBy<T>(__: Placeholder, a: T): {
+  // minBy(__, a)(fn)(b)
+  (fn: (a: T) => Ord): (b: T) => T;
+  // minBy(__, a)(__, b)(fn)
+  (__: Placeholder, b: T): (fn: (a: T) => Ord) => T;
+  // minBy(__, a)(fn, b)
+  (fn: (a: T) => Ord, b: T): T;
+};
+// minBy(fn, a)(b)
+export function minBy<T>(fn: (a: T) => Ord, a: T): (b: T) => T;
+// minBy(__, __, b)
+export function minBy<T>(__: Placeholder, __2: Placeholder, b: T): {
+  // minBy(__, __, b)(fn)(a)
+  (fn: (a: T) => Ord): (a: T) => T;
+  // minBy(__, __, b)(__, a)(fn)
+  (__: Placeholder, a: T): (fn: (a: T) => Ord) => T;
+  // minBy(__, __, b)(fn, a)
+  (fn: (a: T) => Ord, a: T): T;
+};
+// minBy(__, a, b)(fn)
+export function minBy<T>(__: Placeholder, a: T, b: T): (fn: (a: T) => Ord) => T;
+// minBy(fn, __, b)
+export function minBy<T>(fn: (a: T) => Ord, __: Placeholder, b: T): (a: T) => T;
+// minBy(fn, a, b)
+export function minBy<T>(fn: (a: T) => Ord, a: T, b: T): T;
