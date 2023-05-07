21d98a971e60478ca6e86b06aa997b346ee04455
harris-miller <harrismillerconsulting@gmail.com>
2023-05-07 01:13:53 -0600

    WIP

---
+++ test/min.test.ts
@@ -0,0 +1,21 @@
+import { expectType } from 'tsd';
+
+import { __, min } from '../es';
+
+// please note how literals work in this situation
+expectType<1 | 2>(min(1, 2));
+// using variables that are type `number` is the more general use-case here
+const a: number = 1;
+const b: number = 2;
+expectType<number>(min(a, b));
+// so I don't have to make a bunch of variables, we can just do that cast inline
+expectType<number>(min(1 as number, 2 as number));
+// now check the other Ord types
+expectType<string>(min('a' as string, 'b' as string));
+expectType<boolean>(min(true as boolean, false as boolean));
+expectType<Date>(min(new Date(Date.now() - 1), new Date(Date.now())));
+
+// Placeholder
+expectType<number>(min(__, b)(a));
+// curried
+expectType<(b: number) => number>(min(a));
--- types/flip.d.ts
+++ types/flip.d.ts
@@ -7,3 +7,4 @@ export function flip<T, U, TResult>(fn: (arg0: T, arg1: U) => TResult): {
 export function flip<F extends (...args: any) => any, P extends _.F.Parameters<F>>(
   fn: F,
 ): _.F.Curry<(...args: _.T.Merge<[P[1], P[0]], P>) => _.F.Return<F>>;
+export function flip<T, U, TResult>(fn: (arg0: T, arg1: U) => TResult): (arg1: U, arg0?: T) => TResult;
