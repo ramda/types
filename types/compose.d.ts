aad16906fc2f39e703780ba3507f6f718fb38fdd
harris-miller <harrismillerconsulting@gmail.com>
2023-05-09 21:25:00 -0600

    actually, need those to be that way

--- types/compose.d.ts
+++ types/compose.d.ts
@@ -39,11 +39,16 @@ export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f1: (...args: TArgs) => R1,
 ): (...args: TArgs) => R7;
 // fallback overload that covers anything greater than 7, but doesn't type check between functions
-export function compose<TArgs extends any[], R1, TResult>(
+export function compose<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
   ...func: [
     fnLast: (a: any) => TResult,
     ...func: Array<(a: any) => any>,
+    f7: (a: R6) => R7,
+    f6: (a: R5) => R6,
+    f5: (a: R4) => R5,
+    f4: (a: R3) => R4,
+    f3: (a: R2) => R3,
+    f2: (a: R1) => R2,
     f1: (...args: TArgs) => R1
   ]
 ): (...args: TArgs) => TResult;
-
--- types/pipe.d.ts
+++ types/pipe.d.ts
@@ -39,9 +39,15 @@ export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7>(
   f7: (a: R6) => R7,
 ): (...args: TArgs) => R7;
 // fallback overload that covers anything greater than 7, but doesn't type check between functions
-export function pipe<TArgs extends any[], R1, TResult>(
+export function pipe<TArgs extends any[], R1, R2, R3, R4, R5, R6, R7, TResult>(
   ...funcs: [
     f1: (...args: TArgs) => R1,
+    f2: (a: R1) => R2,
+    f3: (a: R2) => R3,
+    f4: (a: R3) => R4,
+    f5: (a: R4) => R5,
+    f6: (a: R5) => R6,
+    f7: (a: R6) => R7,
     ...func: Array<(a: any) => any>,
     fnLast: (a: any) => TResult
   ]
