51b287c4bd473e95d4b491eef6ebaba4bf2e639c
harris-miller <harrismillerconsulting@gmail.com>
2023-05-25 03:26:03 -0600

    that's better

--- types/pluck.d.ts
+++ types/pluck.d.ts
@@ -1,5 +1,7 @@
 import { Placeholder } from 'ramda';
 
-export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): <U extends Record<K, any>>(list: U[]) => Array<U[K]>;
-export function pluck<U>(__: Placeholder, list: U[]): <K extends keyof U>() => Array<U[K]>;
-export function pluck<K extends keyof U, U>(p: K, list: readonly U[]): Array<U[K]>;
+export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
+  <U extends unknown[] | Record<K, any>>(list: U[]): U extends (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
+};
+export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
+export function pluck<K extends keyof U, U>(prop: K, list: readonly U[]): Array<U[K]>;
