cc7ae0ade2e703d70e3d01b971e73ac04f7c362c
harris-miller <harrismillerconsulting@gmail.com>
2023-05-25 03:32:30 -0600

    support objects

--- types/pluck.d.ts
+++ types/pluck.d.ts
@@ -1,7 +1,10 @@
 import { Placeholder } from 'ramda';
 
 export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
+  <U extends readonly unknown[] | Record<K, any>>(obj: Record<PropertyKey, U>): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
   <U extends readonly unknown[] | Record<K, any>>(list: U[]): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
 };
+export function pluck<U>(__: Placeholder, obj: Record<PropertyKey, U>): <K extends keyof U>(prop: K) => Array<U[K]>;
 export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
+export function pluck<K extends keyof U, U>(prop: K, obj: Record<PropertyKey, U>): Array<U[K]>;
 export function pluck<K extends keyof U, U>(prop: K, list: readonly U[]): Array<U[K]>;
