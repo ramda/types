2f527b9813dd3c6d94e6ed823d8c77e3ae03e37d
harris-miller <harrismillerconsulting@gmail.com>
2023-05-25 03:29:24 -0600

    don't forget the readonlys

--- types/pluck.d.ts
+++ types/pluck.d.ts
@@ -1,7 +1,7 @@
 import { Placeholder } from 'ramda';
 
 export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
-  <U extends unknown[] | Record<K, any>>(list: U[]): U extends (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
+  <U extends readonly unknown[] | Record<K, any>>(list: U[]): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
 };
 export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
 export function pluck<K extends keyof U, U>(prop: K, list: readonly U[]): Array<U[K]>;
