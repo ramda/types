3ffc083c031b9b7c00f566a57ca566c19e9961da
harris-miller <harrismillerconsulting@gmail.com>
2023-05-27 16:42:49 -0600

    simplify

--- types/prop.d.ts
+++ types/prop.d.ts
@@ -1,18 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Placeholder } from './util/tools';
 
-// placeholder
-export function prop<U>(__: Placeholder, obj: U): {
-  // key known
-  <_, K extends keyof U>(key: K): U[K];
-  // key unknown, return value settable in generic
-  <V>(key: PropertyKey): V | undefined;
-};
-// most common use case, when key is known on the object
-export function prop<K extends keyof U, U>(key: K, obj: U): U[K];
-// if the key is not known, fall back to this, which defaultly returns unknown, but the generic allows for user to set the return value
-export function prop<V>(key: PropertyKey, obj: unknown): V | undefined;
-// key known, `_` is to keep typescript from getting confused when trying to use the type below
-export function prop<_, K extends PropertyKey>(key: K): <U>(obj: U) => U extends (Record<K, infer T> | Array<infer T>) ? T : never;
-// key unknown
-export function prop<V>(key: PropertyKey): (obj: unknown) => V | unknown;
+// prop(key)(obj)
+export function prop<K extends PropertyKey>(prop: K extends Placeholder ? never : K): <U extends Record<K, any>>(obj: U) => U[K];
+// prop(__, obj))key
+export function prop<U>(__: Placeholder, obj: U): <K extends keyof U>(prop: K) => U[K];
+// prop(key, obj)
+export function prop<K extends keyof U, U>(prop: K extends Placeholder ? never : K, obj: U): U[K];
