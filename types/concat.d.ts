0bbd4998aabc67feaa137626c8347d1514bcf31c
harris-miller <harrismillerconsulting@gmail.com>
2023-05-27 19:38:00 -0600

    that should do it with the typing

--- types/concat.d.ts
+++ types/concat.d.ts
@@ -4,13 +4,14 @@ import { Placeholder } from './util/tools';
 export function concat<S1 extends string>(s1: S1): <S2 extends string>(s2: S2) => `${S1}${S2}`;
 export function concat(s1: string): (s2: string) => string;
 // concat(list)(list)
-export function concat<L1 extends any[]>(list1: L1): <L2 extends any[]>(list2: L2) => [...L1, ...L2];
+export function concat<T>(list1: T[]): (list2: T[]) => T[];
 // concat(__, string)(string)
 export function concat<S2 extends string>(__: Placeholder, s2: S2): <S1 extends string>(s1: S1) => `${S1}${S2}`;
+export function concat(__: Placeholder, s2: string): (s1: string) => string;
 // concat(__, list)(list)
-export function concat<L2 extends any[]>(__: Placeholder,list2: L2): <L1 extends any[]>(list1: L1) => [...L1, ...L2];
+export function concat<T>(__: Placeholder, list2: T[]): (list1: T[]) => T[];
 // concat(string, string)
 export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): `${S1}${S2}`;
 export function concat(s1: string, s2: string): string;
 // concat(list, list)
-export function concat<L1 extends any[], L2 extends any[]>(list1: L1, list2: L2): [...L1, ...L2];
+export function concat<T>(list1: T[], list2: T[]): T[];
