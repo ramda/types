f2b3dc81b2d01846b52cc977c16d0add5251a74a
harris-miller <harrismillerconsulting@gmail.com>
2023-05-27 19:45:26 -0600

    tests

--- test/count.test.ts
+++ test/count.test.ts
@@ -1,8 +1,11 @@
-import { expectType, expectError } from 'tsd';
-import { count } from '../es';
+import { expectType, expectError, expectAssignable } from 'tsd';
+import { count, isNotNil } from '../es';
 
 expectType<number>(count(x => x < 5, [1, 2, 3, 4, 5]));
 
 // expectError when types don't match
-expectError(count(x => x < 5, ['a', 'b', 'c']));
-expectError(count(x => x === 'ok', [1, 2, 3, 4, 5]));
+expectError(count((x: number) => x < 5, ['a', 'b', 'c']));
+expectError(count((x: string) => x === 'ok', [1, 2, 3, 4, 5]));
+
+// this only works with expectAssignable, not expectType, no idea why
+expectAssignable<<T>(list: readonly T[]) => number>(count(isNotNil));
