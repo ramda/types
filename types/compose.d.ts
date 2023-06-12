c834caeef85061f5bdab049222351fbb93a2f327
harris-miller <harrismillerconsulting@gmail.com>
2023-06-12 13:03:09 -0600

    lol oops

--- types/compose.d.ts
+++ types/compose.d.ts
@@ -1,26 +1,26 @@
-export function pipe<A extends ReadonlyArray<unknown>, B>(
+export function compose<A extends ReadonlyArray<unknown>, B>(
   fn1: (...args: A) => B,
 ): (...args: A) => B;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C>(
+export function compose<A extends ReadonlyArray<unknown>, B, C>(
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => C;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D>(
+export function compose<A extends ReadonlyArray<unknown>, B, C, D>(
   fn3: (arg: C) => D,
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => D;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E>(
+export function compose<A extends ReadonlyArray<unknown>, B, C, D, E>(
   fn4: (arg: D) => E,
   fn3: (arg: C) => D,
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => E;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
+export function compose<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
   fn5: (arg: E) => F,
   fn4: (arg: D) => E,
   fn3: (arg: C) => D,
@@ -28,7 +28,7 @@ export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
   fn1: (...args: A) => B,
 ): (...args: A) => F;
 
-export function pipe<
+export function compose<
   A extends ReadonlyArray<unknown>, B, C, D, E, F, G>(
   fn6: (arg: F) => G,
   fn5: (arg: E) => F,
@@ -38,7 +38,7 @@ export function pipe<
   fn1: (...args: A) => B,
 ): (...args: A) => G;
 
-export function pipe<
+export function compose<
   A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H>(
   fn7: (arg: G) => H,
   fn6: (arg: F) => G,
@@ -49,7 +49,7 @@ export function pipe<
   fn1: (...args: A) => B,
 ): (...args: A) => H;
 
-export function pipe<
+export function compose<
   A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I>(
   fn8: (arg: H) => I,
   fn7: (arg: G) => H,
@@ -61,7 +61,7 @@ export function pipe<
   fn1: (...args: A) => B,
 ): (...args: A) => I;
 
-export function pipe<
+export function compose<
   A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I, J>(
   fn9: (arg: I) => J,
   fn8: (arg: H) => I,
