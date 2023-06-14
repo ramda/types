1b559043d868af5449c83e9d502baee5bd3fe42a
harris-miller <harrismillerconsulting@gmail.com>
2023-06-13 22:55:30 -0600

    remove uneeded `extends Placeholder`

--- types/prop.d.ts
+++ types/prop.d.ts
@@ -6,4 +6,4 @@ export function prop<K extends PropertyKey>(prop: K extends Placeholder ? never 
 // prop(__, obj)(key)
 export function prop<U>(__: Placeholder, obj: U): <K extends keyof U>(prop: K) => U[K];
 // prop(key, obj)
-export function prop<K extends keyof U, U>(prop: K extends Placeholder ? never : K, obj: U): U[K];
+export function prop<K extends keyof U, U>(prop: K, obj: U): U[K];
