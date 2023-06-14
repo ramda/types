7433bf7db45661223a9941fc7a03438e9f695b67
harris-miller <harrismillerconsulting@gmail.com>
2023-06-13 21:30:13 -0600

    fix comment

--- types/prop.d.ts
+++ types/prop.d.ts
@@ -3,7 +3,7 @@ import { Placeholder } from './util/tools';
 
 // prop(key)(obj)
 export function prop<K extends PropertyKey>(prop: K extends Placeholder ? never : K): <U extends any[] | Record<K, any>>(obj: U) => U extends (infer T)[] ? T : U extends Record<K, any> ? U[K] : never;
-// prop(__, obj))key
+// prop(__, obj)(key)
 export function prop<U>(__: Placeholder, obj: U): <K extends keyof U>(prop: K) => U[K];
 // prop(key, obj)
 export function prop<K extends keyof U, U>(prop: K extends Placeholder ? never : K, obj: U): U[K];
