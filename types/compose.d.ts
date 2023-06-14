39ee587076096bfd8e10e7336251301063ce8ace
harris-miller <harrismillerconsulting@gmail.com>
2023-06-14 00:24:56 -0600

    put the fallbacks back in

--- types/compose.d.ts
+++ types/compose.d.ts
@@ -1,26 +1,26 @@
-export function compose<A extends ReadonlyArray<unknown>, B>(
+export function compose<A extends readonly unknown[], B>(
   fn1: (...args: A) => B,
 ): (...args: A) => B;
 
-export function compose<A extends ReadonlyArray<unknown>, B, C>(
+export function compose<A extends readonly unknown[], B, C>(
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => C;
 
-export function compose<A extends ReadonlyArray<unknown>, B, C, D>(
+export function compose<A extends readonly unknown[], B, C, D>(
   fn3: (arg: C) => D,
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => D;
 
-export function compose<A extends ReadonlyArray<unknown>, B, C, D, E>(
+export function compose<A extends readonly unknown[], B, C, D, E>(
   fn4: (arg: D) => E,
   fn3: (arg: C) => D,
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => E;
 
-export function compose<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
+export function compose<A extends readonly unknown[], B, C, D, E, F>(
   fn5: (arg: E) => F,
   fn4: (arg: D) => E,
   fn3: (arg: C) => D,
@@ -28,8 +28,7 @@ export function compose<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
   fn1: (...args: A) => B,
 ): (...args: A) => F;
 
-export function compose<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G>(
+export function compose<A extends readonly unknown[], B, C, D, E, F, G>(
   fn6: (arg: F) => G,
   fn5: (arg: E) => F,
   fn4: (arg: D) => E,
@@ -38,8 +37,7 @@ export function compose<
   fn1: (...args: A) => B,
 ): (...args: A) => G;
 
-export function compose<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H>(
+export function compose<A extends readonly unknown[], B, C, D, E, F, G, H>(
   fn7: (arg: G) => H,
   fn6: (arg: F) => G,
   fn5: (arg: E) => F,
@@ -49,8 +47,7 @@ export function compose<
   fn1: (...args: A) => B,
 ): (...args: A) => H;
 
-export function compose<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I>(
+export function compose<A extends readonly unknown[], B, C, D, E, F, G, H, I>(
   fn8: (arg: H) => I,
   fn7: (arg: G) => H,
   fn6: (arg: F) => G,
@@ -61,8 +58,7 @@ export function compose<
   fn1: (...args: A) => B,
 ): (...args: A) => I;
 
-export function compose<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I, J>(
+export function compose<A extends readonly unknown[], B, C, D, E, F, G, H, I, J>(
   fn9: (arg: I) => J,
   fn8: (arg: H) => I,
   fn7: (arg: G) => H,
@@ -73,3 +69,18 @@ export function compose<
   fn2: (arg: B) => C,
   fn1: (...args: A) => B,
 ): (...args: A) => J;
+
+export function compose<A extends readonly unknown[], B, C, D, E, F, G, H, I, R>(
+  ...funcs: [
+    fnLast: (arg: any) => R,
+    ...func: Array<(a: any) => any>,
+    fn8: (arg: H) => I,
+    fn7: (arg: G) => H,
+    fn6: (arg: F) => G,
+    fn5: (arg: E) => F,
+    fn4: (arg: D) => E,
+    fn3: (arg: C) => D,
+    fn2: (arg: B) => C,
+    fn1: (...args: A) => B
+  ]
+): (...args: A) => R;
--- types/pipe.d.ts
+++ types/pipe.d.ts
@@ -1,26 +1,26 @@
-export function pipe<A extends ReadonlyArray<unknown>, B>(
+export function pipe<A extends readonly unknown[], B>(
   fn1: (...args: A) => B,
 ): (...args: A) => B;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C>(
+export function pipe<A extends readonly unknown[], B, C>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
 ): (...args: A) => C;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D>(
+export function pipe<A extends readonly unknown[], B, C, D>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
 ): (...args: A) => D;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E>(
+export function pipe<A extends readonly unknown[], B, C, D, E>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
   fn4: (arg: D) => E,
 ): (...args: A) => E;
 
-export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
+export function pipe<A extends readonly unknown[], B, C, D, E, F>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
@@ -28,8 +28,7 @@ export function pipe<A extends ReadonlyArray<unknown>, B, C, D, E, F>(
   fn5: (arg: E) => F,
 ): (...args: A) => F;
 
-export function pipe<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G>(
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
@@ -38,8 +37,7 @@ export function pipe<
   fn6: (arg: F) => G,
 ): (...args: A) => G;
 
-export function pipe<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H>(
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G, H>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
@@ -49,8 +47,7 @@ export function pipe<
   fn7: (arg: G) => H,
 ): (...args: A) => H;
 
-export function pipe<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I>(
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G, H, I>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
@@ -61,8 +58,7 @@ export function pipe<
   fn8: (arg: H) => I,
 ): (...args: A) => I;
 
-export function pipe<
-  A extends ReadonlyArray<unknown>, B, C, D, E, F, G, H, I, J>(
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G, H, I, J>(
   fn1: (...args: A) => B,
   fn2: (arg: B) => C,
   fn3: (arg: C) => D,
@@ -73,3 +69,29 @@ export function pipe<
   fn8: (arg: H) => I,
   fn9: (arg: I) => J,
 ): (...args: A) => J;
+
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G, H, I, J>(
+  fn1: (...args: A) => B,
+  fn2: (arg: B) => C,
+  fn3: (arg: C) => D,
+  fn4: (arg: D) => E,
+  fn5: (arg: E) => F,
+  fn6: (arg: F) => G,
+  fn7: (arg: G) => H,
+  fn8: (arg: H) => I,
+  fn9: (arg: I) => J,
+): (...args: A) => J;
+export function pipe<A extends readonly unknown[], B, C, D, E, F, G, H, I, R>(
+  ...funcs: [
+    fn1: (...args: A) => B,
+    fn2: (arg: B) => C,
+    fn3: (arg: C) => D,
+    fn4: (arg: D) => E,
+    fn5: (arg: E) => F,
+    fn6: (arg: F) => G,
+    fn7: (arg: G) => H,
+    fn8: (arg: H) => I,
+    ...func: Array<(a: any) => any>,
+    fnLast: (a: any) => R
+  ]
+): (...args: A) => R;
