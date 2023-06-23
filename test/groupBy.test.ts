b8b57134af7fdb70a49bc5d25d9146cae6e3dbe9
Tim Seckinger <seckinger.tim@gmail.com>
2023-06-23 10:16:26 +0200

    placeholder test case

--- test/groupBy.test.ts
+++ test/groupBy.test.ts
@@ -1,4 +1,6 @@
-import { groupBy } from '../es';
+import { groupBy, __ } from '../es';
+
+// returns optional arrays for the groups
 
 const byGrade = groupBy((student: { score: number; name: string }) => {
   const score = student.score;
@@ -14,3 +16,11 @@ const grouped = byGrade(students);
 (grouped.C ?? []).length;
 // @ts-expect-error
 grouped.C.length;
+
+// accepts a placeholder and later specifying the grouping function
+
+const byGrade2 = groupBy(__, students);
+byGrade2((student: { score: number; name: string }) => {
+  const score = student.score;
+  return score < 65 ? 'F' : score < 70 ? 'D' : score < 80 ? 'C' : score < 90 ? 'B' : 'A';
+});
