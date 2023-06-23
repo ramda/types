b1522ded40e9d26dec392feacbe716f61aa36148
Tim Seckinger <seckinger.tim@gmail.com>
2023-06-23 10:04:50 +0200

    switch the overload order and add support for __: Placeholder
    Co-authored-by: Harris Miller <harrismillerconsulting@gmail.com>

--- types/groupBy.d.ts
+++ types/groupBy.d.ts
@@ -1,2 +1,5 @@
-export function groupBy<T, K extends string = string>(fn: (a: T) => K, list: readonly T[]): Partial<Record<K, T[]>>;
+import { Placeholder } from './util/tools';
+
 export function groupBy<T, K extends string = string>(fn: (a: T) => K): (list: readonly T[]) => Partial<Record<K, T[]>>;
+export function groupBy<T>(__: Placeholder, list: readonly T[]): <K extends string = string>(fn: (a: T) => K) => Partial<Record<K, T[]>>;
+export function groupBy<T, K extends string = string>(fn: (a: T) => K, list: readonly T[]): Partial<Record<K, T[]>>;
