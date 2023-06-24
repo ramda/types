16f94601120808ff16caad2f9526be28d4bd0380
harris-miller <harrismillerconsulting@gmail.com>
2023-06-24 15:26:00 -0600

    update comment

--- test/count.test.ts
+++ test/count.test.ts
@@ -7,5 +7,5 @@ expectType<number>(count(x => x < 5, [1, 2, 3, 4, 5]));
 expectError(count((x: number) => x < 5, ['a', 'b', 'c']));
 expectError(count((x: string) => x === 'ok', [1, 2, 3, 4, 5]));
 
-// this only works with expectAssignable, not expectType, no idea why
+// expectType doesn't work here because the `T` defined in the generic is not the same `T` returned by `count(isNotNil)`
 expectAssignable<<T>(list: readonly T[]) => number>(count(isNotNil));
