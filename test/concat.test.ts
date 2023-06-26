76423b03a41c70e326fb6c4fab963b4284bf1f56
harris-miller <harrismillerconsulting@gmail.com>
2023-06-25 19:05:35 -0600

    better string support

---
+++ test/concat.test.ts
@@ -0,0 +1,19 @@
+import { expectType } from 'tsd';
+
+import { __, concat } from '../es';
+
+expectType<'ABCDEF'>(concat('ABC')('DEF'));
+expectType<'ABCDEF'>(concat(__, 'DEF')('ABC'));
+expectType<'ABCDEF'>(concat('ABC', 'DEF'));
+
+expectType<string>(concat('ABC')('DEF' as string));
+expectType<string>(concat(__, 'DEF' as string)('ABC'));
+expectType<string>(concat('ABC', 'DEF' as string));
+
+expectType<string>(concat('ABC' as string)('DEF' as string));
+expectType<string>(concat(__, 'DEF' as string)('ABC' as string));
+expectType<string>(concat('ABC' as string, 'DEF' as string));
+
+expectType<number[]>(concat([1, 2, 3] as const, [4, 5, 6] as const));
+expectType<number[]>(concat([1, 2, 3] as number[], [4, 5, 6] as number[]));
+expectType<number[]>(concat([1, 2, 3], [4, 5, 6]));
--- types/concat.d.ts
+++ types/concat.d.ts
@@ -1,17 +1,14 @@
 import { Placeholder } from './util/tools';
 
 // concat(string)(string)
-export function concat<S1 extends string>(s1: S1): <S2 extends string>(s2: S2) => `${S1}${S2}`;
-export function concat(s1: string): (s2: string) => string;
+export function concat<S1 extends string>(s1: S1): <S2 extends string>(s2: S2) => string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(list)(list)
-export function concat<T>(list1: T[]): (list2: T[]) => T[];
+export function concat<T>(list1: readonly T[]): (list2: readonly T[]) => T[];
 // concat(__, string)(string)
-export function concat<S2 extends string>(__: Placeholder, s2: S2): <S1 extends string>(s1: S1) => `${S1}${S2}`;
-export function concat(__: Placeholder, s2: string): (s1: string) => string;
+export function concat<S2 extends string>(__: Placeholder, s2: S2): <S1 extends string>(s1: S1) => string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(__, list)(list)
-export function concat<T>(__: Placeholder, list2: T[]): (list1: T[]) => T[];
+export function concat<T>(__: Placeholder, list2: readonly T[]): (list1: readonly T[]) => T[];
 // concat(string, string)
-export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): `${S1}${S2}`;
-export function concat(s1: string, s2: string): string;
+export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(list, list)
-export function concat<T>(list1: T[], list2: T[]): T[];
+export function concat<T>(list1: readonly T[], list2: readonly T[]): T[];
