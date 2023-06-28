2204423de90fb216f04d3bf46a52c2641b97c774
harris-miller <harrismillerconsulting@gmail.com>
2023-06-27 22:06:38 -0600

    revert transduce

--- types/transduce.d.ts
+++ types/transduce.d.ts
@@ -1,5 +1,11 @@
 export function transduce<T, U, V>(
   xf: (arg: readonly T[]) => U[],
+  fn: (acc: V, val: U) => V,
+  acc: V,
+  list: readonly T[],
+): V;
+export function transduce<T, U, V>(
+  xf: (arg: readonly T[]) => U[],
 ): (fn: (acc: V, val: U) => V, acc: V, list: readonly T[]) => V;
 export function transduce<T, U, V>(
   xf: (arg: readonly T[]) => U[],
@@ -10,9 +16,3 @@ export function transduce<T, U, V>(
   fn: (acc: V, val: U) => V,
   acc: readonly T[],
 ): (list: readonly T[]) => V;
-export function transduce<T, U, V>(
-  xf: (arg: readonly T[]) => U[],
-  fn: (acc: V, val: U) => V,
-  acc: V,
-  list: readonly T[],
-): V;
