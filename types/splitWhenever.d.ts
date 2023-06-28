44714c8651ea83027edd70b5fae8cacbacdacf62
harris-miller <harrismillerconsulting@gmail.com>
2023-06-27 20:59:29 -0600

    export not declare

--- types/splitWhenever.d.ts
+++ types/splitWhenever.d.ts
@@ -1,2 +1,2 @@
-declare function splitWhenever<T>(pred: (a: T) => boolean): <T>(list: T[]) => T[][];
-declare function splitWhenever<T>(pred: (a: T) => boolean, list: T[]): T[][];
+export function splitWhenever<T>(pred: (a: T) => boolean): <T>(list: T[]) => T[][];
+export function splitWhenever<T>(pred: (a: T) => boolean, list: T[]): T[][];
