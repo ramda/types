0f65159100a2e28add84ed0b3fee6119c6a7d9a3
harris-miller <harrismillerconsulting@gmail.com>
2023-06-27 18:17:29 -0600

    ramda concat now matches behavior of native concat

--- test/concat.test.ts
+++ test/concat.test.ts
@@ -14,23 +14,27 @@ expectType<string>(concat('ABC' as string)('DEF' as string));
 expectType<string>(concat(__, 'DEF' as string)('ABC' as string));
 expectType<string>(concat('ABC' as string, 'DEF' as string));
 
-expectType<number[]>(concat([1, 2, 3] as const)([4, 5, 6] as const));
-expectType<number[]>(concat(__, [4, 5, 6] as const)([1, 2, 3] as const));
-expectType<number[]>(concat([1, 2, 3] as const, [4, 5, 6] as const));
-
-
-expectType<number[]>(concat([1, 2, 3] as const)([4, 5, 6]));
-expectType<number[]>(concat(__, [4, 5, 6])([1, 2, 3] as const));
-expectType<number[]>(concat([1, 2, 3] as const, [4, 5, 6]));
-
-expectType<number[]>(concat([1, 2, 3])([4, 5, 6] as const));
-expectType<number[]>(concat(__, [4, 5, 6] as const)([1, 2, 3]));
-expectType<number[]>(concat([1, 2, 3], [4, 5, 6] as const));
-
+expectType<number[]>([1, 2, 3].concat([4, 5, 6]));
 expectType<number[]>(concat([1, 2, 3])([4, 5, 6]));
 expectType<number[]>(concat(__, [4, 5, 6])([1, 2, 3]));
 expectType<number[]>(concat([1, 2, 3], [4, 5, 6]));
 
+expectError(([1, 2, 3] as [1, 2, 3]).concat([4, 5, 6]));
+expectError(concat([1, 2, 3] as [1, 2, 3])([4, 5, 6]));
+expectError(concat(__, [4, 5, 6])([1, 2, 3] as [1, 2, 3]));
+expectError(concat([1, 2, 3] as [1, 2, 3], [4, 5, 6]));
+
+expectError(([1, 2, 3] as const).concat([4, 5, 6]));
+expectError(concat([1, 2, 3] as const)([4, 5, 6]));
+expectError(concat(__, [4, 5, 6])([1, 2, 3] as const));
+expectError(concat([1, 2, 3] as const, [4, 5, 6]));
+
+expectType<number[]>([1, 2, 3].concat([4, 5, 6] as const));
+expectType<number[]>(concat([1, 2, 3])([4, 5, 6]  as const));
+expectType<number[]>(concat(__, [4, 5, 6]  as const)([1, 2, 3]));
+expectType<number[]>(concat([1, 2, 3], [4, 5, 6] as const));
+
+// sanity check for arrays of completely different types
 expectError(concat([] as number[])([] as { foo: string }[]));
 expectError(concat(__, [] as { foo: string }[])([] as number[]));
 expectError(concat([] as number[], [] as { foo: string }[]));
--- types/concat.d.ts
+++ types/concat.d.ts
@@ -1,14 +1,14 @@
-import { Placeholder, WidenLiterals } from './util/tools';
+import { Placeholder } from './util/tools';
 
 // concat(string)(string)
 export function concat<S1 extends string>(s1: S1): <S2 extends string>(s2: S2) => string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(list)(list)
-export function concat<T>(list1: readonly T[]): (list2: readonly WidenLiterals<T>[]) => WidenLiterals<T>[];
+export function concat<T>(list1: readonly T[]): (list2: readonly T[]) => T[];
 // concat(__, string)(string)
 export function concat<S2 extends string>(__: Placeholder, s2: S2): <S1 extends string>(s1: S1) => string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(__, list)(list)
-export function concat<T>(__: Placeholder, list2: readonly T[]): (list1: readonly WidenLiterals<T>[]) => WidenLiterals<T>[];
+export function concat<T2>(__: Placeholder, list2: readonly T2[]): <T1>(list1: (readonly T2[] extends readonly T1[] ? readonly T1[] : never)) => T1[];
 // concat(string, string)
 export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(list, list)
-export function concat<T>(list1: readonly T[], list2: readonly T[]): WidenLiterals<T>[];
+export function concat<T1, T2 extends T1>(list1: readonly T1[], list2: readonly T2[]): T1[];
