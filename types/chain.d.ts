56e7d2ba71f8f33cff7be7d8ef444a60cddf4090
harris-miller <harrismillerconsulting@gmail.com>
2023-06-27 21:48:40 -0600

    revert chain, failing existing DefinitelyTyped hard

--- types/chain.d.ts
+++ types/chain.d.ts
@@ -1,15 +1,16 @@
-// chain(fn)(monad)
-export function chain<A, Ma extends { chain: (fn: (a: A) => Mb) => Mb }, Mb>(fn: (a: A) => Mb): (monad: Ma) => Mb;
-// chain (f)(g)(x)
-export function chain<A, B, R>(aToMb: (a: A, r: R) => B): (Ma: (r: R) => A) => (r: R) => B;
+// chain(fn, list)
+export function chain<A, B>(fn: (n: A) => readonly B[], list: readonly A[]): B[];
 // chain(fn)(list)
 export function chain<A, B>(fn: (n: A) => readonly B[]): (list: readonly A[]) => B[];
 
 // chain(fn, monad)
 export function chain<A, Ma extends { chain: (fn: (a: A) => Mb) => Mb }, Mb>(fn: (a: A) => Mb, monad: Ma): Mb;
+// chain(fn)(monad)
+export function chain<A, Ma extends { chain: (fn: (a: A) => Mb) => Mb }, Mb>(fn: (a: A) => Mb): (monad: Ma) => Mb;
+
 // chain (f, g)(x)
 export function chain<A, B, R>(aToMb: (a: A, r: R) => B, Ma: (r: R) => A): (r: R) => B;
-// chain(fn, list)
-export function chain<A, B>(fn: (n: A) => readonly B[], list: readonly A[]): B[];
+// chain (f)(g)(x)
+export function chain<A, B, R>(aToMb: (a: A, r: R) => B): (Ma: (r: R) => A) => (r: R) => B;
 
 // TODO: types for transducer variation
