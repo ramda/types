5a099d1c3c1229dae4cc78b2d9b95d82c7f2edc9
harris-miller <harrismillerconsulting@gmail.com>
2023-06-27 19:29:45 -0600

    added a comment

--- types/concat.d.ts
+++ types/concat.d.ts
@@ -11,4 +11,5 @@ export function concat<T2>(__: Placeholder, list2: readonly T2[]): <T1>(list1: (
 // concat(string, string)
 export function concat<S1 extends string, S2 extends string>(s1: S1, s2: S2): string extends (S1 | S2) ? string : `${S1}${S2}`;
 // concat(list, list)
+// if you don't do 2 types here the single T will collapse list1 and list2 when you have tuples of the same type, which is incorrect behavior
 export function concat<T1, T2 extends T1>(list1: readonly T1[], list2: readonly T2[]): T1[];
