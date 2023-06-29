0d4ce1af3e97f81f44a389b9ace9750e61602196
Tim Seckinger <seckinger.tim@gmail.com>
2023-06-29 04:39:23 +0200

    fix(groupBy): return Partial Record (#45)

---
+++ test/groupBy.test.ts
@@ -0,0 +1,29 @@
+import { expectType } from 'tsd';
+import { groupBy, __ } from '../es';
+
+// returns optional arrays for the groups
+
+const byGrade = groupBy((student: { score: number; name: string }) => {
+  const score = student.score;
+  return score < 65 ? 'F' : score < 70 ? 'D' : score < 80 ? 'C' : score < 90 ? 'B' : 'A';
+});
+const students = [
+  { name: 'Abby', score: 84 },
+  { name: 'Eddy', score: 58 },
+  { name: 'Jack', score: 69 }
+];
+
+const grouped = byGrade(students);
+expectType<{ score: number; name: string }[] | undefined>(grouped.C);
+(grouped.C ?? []).length;
+// @ts-expect-error
+grouped.C.length;
+
+// accepts a placeholder and later specifying the grouping function
+
+const byGrade2 = groupBy(__, students);
+const grouped2 = byGrade2((student: { score: number; name: string }) => {
+  const score = student.score;
+  return score < 65 ? 'F' : score < 70 ? 'D' : score < 80 ? 'C' : score < 90 ? 'B' : 'A';
+});
+expectType<{ score: number; name: string }[] | undefined>(grouped2.C);
