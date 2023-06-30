949ab3bdc44dd3e40e32ea1c2d9191bc4a6532d8
harris-miller <harrismillerconsulting@gmail.com>
2023-06-30 02:05:28 -0600

    fix adjust while we're here

--- test/adjust.test.ts
+++ test/adjust.test.ts
@@ -1,12 +1,44 @@
 import { expectType, expectError } from 'tsd';
-import { adjust, isNil, toUpper } from '../es';
+import { __, adjust, identity, toUpper } from '../es';
 
-expectType<string[]>(adjust(1, toUpper, ['a', 'b', 'c']));
+// adjust(index)(fn)(list)
+expectType<string[]>(adjust(1)(toUpper)(['a', 'b', 'c']));
+// adjust(index)(__, list)(fn)
+expectType<string[]>(adjust(1)(__, ['a', 'b', 'c'])(toUpper));
+// adjust(index)(fn, list)
+expectType<string[]>(adjust(1)(toUpper, ['a', 'b', 'c']));
+
+// adjust(__, fn)(index)(list)
+expectType<string[]>(adjust(__, toUpper)(1)(['a', 'b', 'c']));
+// adjust(__, fn)(__, list)(index)
+expectType<string[]>(adjust(__, toUpper)(__, ['a', 'b', 'c'])(1));
+// adjust(__, fn)(index), list)
+expectType<string[]>(adjust(__, toUpper)(1, ['a', 'b', 'c']));
+
+// adjust(index, fn)(index)
 expectType<string[]>(adjust(2, toUpper)(['c', 'd', 'e']));
 
+// adjust(__, __, list)(index)(fn)
+expectType<string[]>(adjust(__, __, ['a', 'b', 'c'])(1)(toUpper));
+// adjust(__, __, list)(__, fn)(index)
+expectType<string[]>(adjust(__, __, ['a', 'b', 'c'])(__, toUpper)(1));
+// adjust(__, __, index)(index, fn)
+expectType<string[]>(adjust(__, __, ['a', 'b', 'c'])(1, toUpper));
+
+// adjust(index, fn, index)
+expectType<string[]>(adjust(1, toUpper, ['a', 'b', 'c']));
+
+expectType<string[]>(adjust(1)(identity)(['a', 'b', 'c']));
+expectType<string[]>(adjust(1, identity)(['a', 'b', 'c']));
+expectType<string[]>(adjust(1, identity, ['a', 'b', 'c']));
+
+// some other types
 expectType<number[]>(adjust(2, (n: number) => n * 2)([1, 2, 3]));
 expectType<boolean[]>(adjust(2, (n: boolean) => !n)([true, false, true]));
 
-expectError(adjust(1, toString, [1, 2, 3]));
+// fail if `fn` returns different type
+expectError(adjust(1, (x: number) => x.toString(), [1, 2, 3]));
+// fail if index is not a number
 expectError(adjust('1', toUpper, ['c', 'd', 'e']));
-expectError(adjust(2, isNil)([2, 3, 4]));
+// fail is `fn` returns void
+expectError(adjust(2, () => {}, [2, 3, 4]));
--- types/adjust.d.ts
+++ types/adjust.d.ts
@@ -2,31 +2,26 @@ import { Placeholder } from './util/tools';
 
 // adjust(index)
 export function adjust(index: number): {
-  // adjust(index)(fn, list)
-  <T>(fn: (a: T) => T, list: readonly T[]): T[];
-  // adjust(index)(__, list)(fn)
-  <T>(__: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
   // adjust(index)(fn)(list)
   <T>(fn: (a: T) => T): (list: readonly T[]) => T[];
+  // adjust(index)(__, list)(fn)
+  <T>(__: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
+  // adjust(index)(fn, list)
+  <T>(fn: (a: T) => T, list: readonly T[]): T[];
 };
 
 // adjust(__, fn)
 export function adjust<T>(__: Placeholder, fn: (a: T) => T): {
-  // adjust(__, fn)(list)(index)
-  (list: readonly T[]): (index: number) => T[];
-  // adjust(__, fn)(__, index)(list)
-  (__: Placeholder, index: number): (list: readonly T[]) => T[];
-  // adjust(__, fn)(list, index)
-  (list: readonly T[], index: number): T[];
+  // adjust(__, fn)(index)(list)
+  (index: number): (list: readonly T[]) => T[];
+  // adjust(__, fn)(__, list)(index)
+  (__: Placeholder, list: readonly T[]): (index: number) => T[];
+  // adjust(__, fn)(index, list)
+  (index: number, list: readonly T[]): T[];
 };
 
-export function adjust<T>(index: number, fn: (a: T) => T): (list: readonly T[]) => T[];
-
 // adjust(index, fn)(list)
 export function adjust<T>(index: number, fn: (a: T) => T): (list: readonly T[]) => T[];
-// adjust(index, fn)(list)
-export function adjust<T>(index: number, fn: (a: T) => T): (list: readonly T[]) => T[];
-
 
 // adjust(__, __, list)
 export function adjust<T>(__: Placeholder, __2: Placeholder, list: readonly T[]): {
@@ -37,10 +32,11 @@ export function adjust<T>(__: Placeholder, __2: Placeholder, list: readonly T[])
   // adjust(__, __, list)(index, fn)
   (index: number, fn: (a: T) => T): T[];
 };
-// adjust(index, __, list)(fn)
-export function adjust<T>(index: number, __: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
+
 // adjust(__, fn, list)(index)
 export function adjust<T>(__: Placeholder, fn: (a: T) => T, list: readonly T[]): (index: number) => T[];
+// adjust(index, __, list)(fn)
+export function adjust<T>(index: number, __: Placeholder, list: readonly T[]): (fn: (a: T) => T) => T[];
 
 // adjust(index, fn, list)
 export function adjust<T>(index: number, fn: (a: T) => T, list: readonly T[]): T[];
