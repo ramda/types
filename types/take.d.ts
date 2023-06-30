c6af24ae05b2dd04b6459e892a77bf20075e75fe
harris-miller <harrismillerconsulting@gmail.com>
2023-06-30 02:53:56 -0600

    re-order take overloads

--- types/take.d.ts
+++ types/take.d.ts
@@ -2,5 +2,5 @@ export function take(n: number): {
   (xs: string): string;
   <T>(xs: readonly T[]): T[];
 };
-export function take<T>(n: number, xs: readonly T[]): T[];
 export function take(n: number, xs: string): string;
+export function take<T>(n: number, xs: readonly T[]): T[];
