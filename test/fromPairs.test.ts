081380d92930ae4952547a225875c537019df1be
harris-miller <harrismillerconsulting@gmail.com>
2023-07-01 22:15:05 -0600

    fromPairs K extends PropertyKey

--- test/fromPairs.test.ts
+++ test/fromPairs.test.ts
@@ -2,10 +2,12 @@ import { expectType } from 'tsd';
 
 import { fromPairs } from '../es';
 
+const symbolKey = Symbol('key');
+
 // literals
-expectType<{ foo: number; bar: number }>(fromPairs([['foo', 1], ['bar', 2]]));
+expectType<{ foo: number; bar: number; }>(fromPairs([['foo', 1], ['bar', 2]]));
 // typed
-expectType<{ foo: number; bar: number }>(fromPairs([] as ['foo' | 'bar', number][]));
+expectType<{ foo: number; bar: number; 0: number; [symbolKey]: number; }>(fromPairs([] as ['foo' | 'bar' | 0 | typeof symbolKey, number][]));
 // keys that are not a union of values produced an index signature type
 expectType<{ [x: string]: number }>(fromPairs([] as [string, number][]));
 // 'foo' | 'bar' | string collapses to string, this is expected
--- types/fromPairs.d.ts
+++ types/fromPairs.d.ts
@@ -1,3 +1,3 @@
-export function fromPairs<K extends string | number, V>(
+export function fromPairs<K extends PropertyKey, V>(
   pairs: readonly [K, V][]
 ): { [P in K]: V };
