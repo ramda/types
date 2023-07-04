cb68debe619a9d95d38b0958f3c61601d570d9d2
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 02:26:43 -0600

    remove array support

--- test/prop.test.ts
+++ test/prop.test.ts
@@ -1,6 +1,6 @@
 import { expectError, expectType } from 'tsd';
 
-import { __, find, pipe, prop } from '../es';
+import { __, find, isNil, pipe, prop, tap } from '../es';
 
 type Foo = {
   a: string;
@@ -14,10 +14,6 @@ expectType<number>(prop('b')({} as Foo));
 // reject keys unknown in either direction
 expectError(prop('c')({} as Foo));
 expectError(prop('a')({ c: 'error' }));
-// support arrays
-expectType<number>(prop(0)([1, 2, 3, 4]));
-// all numbers work as keys, no guarantee it will return a value
-expectType<number>(prop(10)([1, 2, 3, 4]));
 
 // prop(__, obj)(key)
 expectType<string>(prop(__, {} as Foo)('a'));
@@ -25,10 +21,6 @@ expectType<number>(prop(__, {} as Foo)('b'));
 // reject keys unknown in either direction
 expectError(prop(__, {} as Foo)('c'));
 expectError(prop(__, { c: 'error' })('a'));
-// support arrays
-expectType<number>(prop(__, [1, 2, 3, 4])(0));
-// all numbers work as keys, no guarantee it will return a value
-expectType<number>(prop(__, [1, 2, 3, 4])(10));
 
 // prop(prop, obj)
 expectType<string>(prop('a', {} as Foo));
@@ -36,10 +28,6 @@ expectType<number>(prop('b', {} as Foo));
 // reject keys unknown in either direction
 expectError(prop('c', {} as Foo));
 expectError(prop('a', { c: 'error' }));
-// support arrays
-expectType<number>(prop(0, [1, 2, 3, 4]));
-// all numbers work as keys, no guarantee it will return a value
-expectType<number>(prop(10, [1, 2, 3, 4]));
 
 //
 // testing and example of unions with `undefined`
@@ -58,5 +46,14 @@ expectError(pipe(
 // add in non-null assertion to have it pass
 expectType<string>(pipe(
   (todos: Todo[]) => find(todo => todo.isDone, todos)!,
+  prop('description')
+)(todos));
+
+// or `asserts` with a `tap`
+expectType<string>(pipe(
+  (todos: Todo[]) => find(todo => todo.isDone, todos)!,
+  tap((x: Todo): asserts x => {
+    if (isNil(x)) { throw new Error("Couldn't find Todo"); }
+  }),
   prop('description')
 )(todos));
