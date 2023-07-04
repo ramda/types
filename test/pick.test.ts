6947acef532263d7d5239a044888a82af9d18a53
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 15:47:04 -0600

    tests and Record support

--- test/pick.test.ts
+++ test/pick.test.ts
@@ -1,7 +1,18 @@
-import { expectType } from 'tsd';
+import { expectType, expectError } from 'tsd';
 
 import { pick } from '../es';
 
 const obj = { foo: 1, bar: '2', biz: false };
 
-expectType<{ foo: number, bar: string }>(pick(['foo', 'bar'], obj));
+expectType<{ foo: number; }>(pick(['foo'])(obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'])(obj));
+expectType<{ foo: number; bar: string; biz: boolean }>(pick(['foo', 'bar', 'biz'])(obj));
+expectError(pick(['baz', 'bar', 'biz'])(obj));
+
+expectType<{ foo: number; }>(pick(['foo'], obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'], obj));
+expectType<{ foo: number; bar: string; biz: boolean }>(pick(['foo', 'bar', 'biz'], obj));
+expectError(pick(['baz', 'bar', 'biz'], obj));
+
+// Record
+expectType<Record<string, number>>(pick(['foo', 'bar'], {} as Record<string, number>));
