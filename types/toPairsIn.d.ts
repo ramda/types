5d87c8c0e5e10831885f5f88a7d3322001759d04
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 02:54:19 -0600

    toPairsIn just returns array of [string, S] tuples

--- types/toPairsIn.d.ts
+++ types/toPairsIn.d.ts
@@ -1,4 +1,1 @@
-export function toPairsIn<O extends object, K extends Extract<keyof O, string | number>>(
-  obj: O,
-): Array<{ [key in K]: [`${key}`, O[key]] }[K]>;
-export function toPairsIn<S>(obj: Record<string | number, S>): Array<[string, S]>;
+export function toPairsIn<S>(obj: Record<PropertyKey, S>): Array<[string, S]>;
