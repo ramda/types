8f4220d34bc5b14a2f3fe1b3c1c7e2293b4a149f
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 13:48:44 -0600

    dissollow placeholder in that way

--- types/dissoc.d.ts
+++ types/dissoc.d.ts
@@ -1,6 +1,6 @@
 import { Placeholder } from './util/tools';
 
 // `string extends keyof U` is to support `Record<string, T>`
-export function dissoc<K extends PropertyKey>(prop: K): <U extends { [P in K]?: any}>(obj: string extends keyof U ? U : undefined extends U[K] ? U : never) => U;
+export function dissoc<K extends PropertyKey>(prop: K extends Placeholder ? never : K): <U extends { [P in K]?: any}>(obj: string extends keyof U ? U : undefined extends U[K] ? U : never) => U;
 export function dissoc<U>(__: Placeholder, obj: U): <K extends keyof U>(prop: string extends keyof U ? K : undefined extends U[K] ? K : never) => U;
 export function dissoc<U, K extends keyof U>(prop: string extends keyof U ? K : undefined extends U[K] ? K : never, obj: U): U;
