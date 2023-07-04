dd5482fcfcd62f7181245de3930557836af56367
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 12:16:22 -0600

    rename that type var

--- test/assoc.test.ts
+++ test/assoc.test.ts
@@ -1,19 +1,19 @@
 import { expectType, expectError } from 'tsd';
 import { __, assoc, map } from '../es';
 
-type BasicObj = {
+type Obj = {
   str: string;
   num: number;
 };
 
-const obj: BasicObj = { str: 'foo', num: 1 };
+const obj: Obj = { str: 'foo', num: 1 };
 
 //
 // assoc(key)
 //
 
 // assoc(key)(__, obj)(val)
-expectType<BasicObj>(assoc('str')(__, obj)('bar'));
+expectType<Obj>(assoc('str')(__, obj)('bar'));
 // fails for wrong value type
 expectError(assoc('str')(__, obj)(2));
 // fails if key not unknown
@@ -22,7 +22,7 @@ expectError(assoc('what')(__, obj)('bar'));
 expectType<Record<string, number>>(assoc('what')(__, {} as Record<string, number>)(2));
 
 // assoc(key)(val, obj)
-expectType<BasicObj>(assoc('str')('bar', obj));
+expectType<Obj>(assoc('str')('bar', obj));
 // fails for wrong value type
 expectError(assoc('str')(2, obj));
 // fails if key not unknown
@@ -31,7 +31,7 @@ expectError(assoc('what')('bar', obj));
 expectType<Record<string, number>>(assoc('what')(2, {} as Record<string, number>));
 
 // assoc(key)(val)(obj)
-expectType<BasicObj>(assoc('str')('bar')(obj));
+expectType<Obj>(assoc('str')('bar')(obj));
 // fails for wrong value type
 expectError(assoc('str')(2)(obj));
 // fails if key not unknown
@@ -45,7 +45,7 @@ expectType<Record<string, number>>(assoc('what')(2)({} as Record<string, number>
 //
 
 // assoc(__, val)(key)(obj)
-expectType<BasicObj>(assoc(__, 'bar')('str')(obj));
+expectType<Obj>(assoc(__, 'bar')('str')(obj));
 // fails for wrong value type
 expectError(assoc(__, 2)('str')(obj));
 // fails if key not unknown
@@ -54,7 +54,7 @@ expectError(assoc(__, 'bar')('what')(obj));
 expectType<Record<string, number>>(assoc(__, 2)('what')({} as Record<string, number>));
 
 // assoc(__, val)(__, key)(obj)
-expectType<BasicObj>(assoc(__, 'bar')(__, obj)('str'));
+expectType<Obj>(assoc(__, 'bar')(__, obj)('str'));
 // fails for wrong value type
 expectError(assoc(__, 2)(__, obj)('str'));
 // fails if key not unknown
@@ -63,7 +63,7 @@ expectError(assoc(__, 'bar')(__, obj)('what'));
 expectType<Record<string, number>>(assoc(__, 2)(__, {} as Record<string, number>)('str'));
 
 // assoc(__, val)(key, obj)
-expectType<BasicObj>(assoc(__, 'bar')('str', obj));
+expectType<Obj>(assoc(__, 'bar')('str', obj));
 // fails for wrong value type
 expectError(assoc(__, 2)('str', obj));
 // fails if key not unknown
@@ -76,7 +76,7 @@ expectType<Record<string, number>>(assoc(__, 2)('str', {} as Record<string, numb
 //
 
 // assoc(key, val)(obj)
-expectType<BasicObj>(assoc('str', 'bar')(obj));
+expectType<Obj>(assoc('str', 'bar')(obj));
 // fails for wrong value type
 expectError(assoc('str', 2)(obj));
 // fails if key not unknown
@@ -89,7 +89,7 @@ expectType<Record<string, number>>(assoc('str', 2)({} as Record<string, number>)
 //
 
 // assoc(__, __, obj)(key)(val)
-expectType<BasicObj>(assoc(__, __, obj)('str')('bar'));
+expectType<Obj>(assoc(__, __, obj)('str')('bar'));
 // fails for wrong value type
 expectError(assoc(__, __, obj)('str')(2));
 // fails if key not unknown
@@ -98,7 +98,7 @@ expectError(assoc(__, __, obj)('what')('bar'));
 expectType<Record<string, number>>(assoc(__, __, {} as Record<string, number>)('str')(2));
 
 // assoc(__, __, obj)(__, val)(key)
-expectType<BasicObj>(assoc(__, __, obj)(__, 'bar')('str'));
+expectType<Obj>(assoc(__, __, obj)(__, 'bar')('str'));
 // fails for wrong value type
 expectError(assoc(__, __, obj)(__, 2)('str'));
 // fails if key not unknown
@@ -107,7 +107,7 @@ expectError(assoc(__, __, obj)(__, 'bar')('what'));
 expectType<Record<string, number>>(assoc(__, __, {} as Record<string, number>)(__, 2)('str'));
 
 // assoc(__, __, obj)(key, val)
-expectType<BasicObj>(assoc(__, __, obj)('str', 'bar'));
+expectType<Obj>(assoc(__, __, obj)('str', 'bar'));
 // fails for wrong value type
 expectError(assoc(__, __, obj)('str', 2));
 // fails if key not unknown
@@ -120,7 +120,7 @@ expectType<Record<string, number>>(assoc(__, __, {} as Record<string, number>)('
 //
 
 // assoc(__, val, obj)(prop)
-expectType<BasicObj>(assoc(__,'bar', obj)('str'));
+expectType<Obj>(assoc(__,'bar', obj)('str'));
 // fails for wrong value type
 expectError(assoc(__, 2, obj)('str'));
 // fails if key not unknown
@@ -129,7 +129,7 @@ expectError(assoc(__, 'bar', obj)('what'));
 expectType<Record<string, number>>(assoc(__, 2, {} as Record<string, number>)('str'));
 
 // assoc(key, __, obj)(__, val)
-expectType<BasicObj>(assoc('str', __, obj)('bar'));
+expectType<Obj>(assoc('str', __, obj)('bar'));
 // fails for wrong value type
 expectError(assoc('str', __, obj)(2));
 // fails if key not unknown
@@ -138,7 +138,7 @@ expectError(assoc('what', __, obj)('bar'));
 expectType<Record<string, number>>(assoc('str', __, {} as Record<string, number>)(2));
 
 // assoc(key, val, obj)
-expectType<BasicObj>(assoc('str', 'bar', obj));
+expectType<Obj>(assoc('str', 'bar', obj));
 // fails for wrong value type
 expectError(assoc('str', 2, obj));
 // fails if key not unknown
@@ -150,5 +150,5 @@ expectType<Record<string, number>>(assoc('str', 2, {} as Record<string, number>)
 // map tests for sanity
 //
 
-expectType<BasicObj[]>([obj].map(assoc('str', 'bar')));
-expectType<BasicObj[]>(map(assoc('str', 'bar'), [obj]));
+expectType<Obj[]>([obj].map(assoc('str', 'bar')));
+expectType<Obj[]>(map(assoc('str', 'bar'), [obj]));
