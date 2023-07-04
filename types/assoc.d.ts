3765081f57aafb227fe1c7f778c849bb86a2cfb8
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 03:09:34 -0600

    type safety!

--- types/assoc.d.ts
+++ types/assoc.d.ts
@@ -1,64 +1,49 @@
-import { Placeholder, AssocResults } from './util/tools';
+import { Placeholder } from './util/tools';
 
 // assoc(prop)
-export function assoc<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
-  // assoc(prop)(val)
-  <T>(val: T): {
-    // assoc(prop)(val)(obj), when obj has key prop, tests if val is typeof obj[prop] for best return type
-    <U>(obj: U): AssocResults<K, T, U>;
-  }
+export function assoc<K extends PropertyKey>(prop: K): {
+  // assoc(prop)(val)(obj)
+  <T>(val: T): <U extends Record<K, T>>(obj: T) => U;
 
-  // assoc(prop)(__, obj)
-  <U>(__: Placeholder, obj: U): {
-    // assoc(prop)(__, obj)(val), when obj has key prop, tests if val is typeof obj[prop] for best return type
-    <T>(val: T): AssocResults<K, T, U>;
-  }
+  // assoc(prop)(__, obj)(val)
+  <U extends Record<K, any>>(__: Placeholder, obj: U): <T extends U[K]>(val: T) => U;
 
   // assoc(prop)(val, obj) when obj has key prop, tests if val is typeof obj[prop] for best return type
-  <T, U>(val: T, obj: U): AssocResults<K, T, U>;
+  <U extends Record<K, T>, T>(val: T, obj: U): U;
 };
 
 // assoc(__, val)
 export function assoc<T>(__: Placeholder, val: T) : {
-  // assoc(__, val)(prop)
-  <K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
-    // assoc(__, val)(prop)(obj)
-    <U>(obj: U): AssocResults<K, T, U>;
-  }
+  // assoc(__, val)(prop)(obj)
+  <K extends PropertyKey>(prop: K extends Placeholder ? never : K): <U extends Record<K, T>>(obj: U) => U;
 
-  // assoc(__, val)(__, obj)
-  <U>(__2: Placeholder, obj: U): {
-    // assoc(__, val)(__, obj)(prop)
-    <K extends PropertyKey>(prop: K): AssocResults<K, T, U>;
-  };
+  // assoc(__, val)(__, obj)(prop)
+  <U>(__2: Placeholder, obj: U): <K extends keyof U>(prop: T extends U[K] ? K : never) => U;
 
   // assoc(__, val)(prop, obj)
-  <K extends PropertyKey, U>(prop: K, obj: U): AssocResults<K, T, U>;
+  <U extends Record<K, T>, K extends keyof U>(prop: K, obj: U);
 };
 
-// assoc(prop, val)
-export function assoc<K extends PropertyKey, T>(prop: K extends Placeholder ? never : K, val: T): {
-  // assoc(prop, val)(obj)
-  <U>(obj: U): AssocResults<K, T, U>;
-};
+// assoc(prop, val)(obj)
+export function assoc<K extends PropertyKey, T>(prop: K extends Placeholder ? never : K, val: T): <U extends Record<K, T>>(obj: U) => U;
 
 // assoc (__, __, obj)
 export function assoc<U>(__: Placeholder, __2: Placeholder, obj: U): {
   // assoc(__, __, obj)(prop)(val)
-  <K extends PropertyKey>(prop: K extends Placeholder ? never : K): <T>(val: T) => AssocResults<K, T, U>;
+  <K extends keyof U>(prop: K extends Placeholder ? never : K): <T extends U[K]>(val: T) => U;
 
   // assoc(__, __, obj)(__, val)(prop)
-  <T>(__: Placeholder, val: T): <K extends PropertyKey>(key: K) => AssocResults<K, T, U>;
+  <T>(__: Placeholder, val: T): <K extends keyof U>(key: T extends U[K] ? T : never) => U;
 
   // assoc(__, __, obj)(prop, val)
-  <K extends PropertyKey, T>(prop: K, val: T): AssocResults<K, T, U>;
+  <K extends keyof U, T extends U[K]>(prop: K, val: T): U;
 };
 
 // assoc(__, val, obj)(prop)
-export function assoc<T, U>(__: Placeholder, val: T, obj: U): <K extends PropertyKey>(prop: K extends Placeholder ? never : K) => AssocResults<K, T, U>;
+export function assoc<U, T extends U[keyof U]>(__: Placeholder, val: T, obj: U): <K extends keyof U>(prop: T extends U[K] ? T : never) => U;
 
 // assoc(prop, __, obj)(val)
-export function assoc<K extends PropertyKey, U>(prop: K, __: Placeholder, obj: U): <T>(val: T) => AssocResults<K, T, U>;
+export function assoc<U, K extends keyof U>(prop: K, __: Placeholder, obj: U): <T extends U[K]>(val: T) => U;
 
 // assoc(prop, val, obj)
-export function assoc<K extends PropertyKey, T, U>(prop: K, val: T, obj: U): AssocResults<K, T, U>;
+export function assoc<U, K extends keyof U, T extends U[K]>(prop: K, val: T, obj: U): U;
