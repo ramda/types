7f03f4b3bd800d5301b1a7d45423beb6b2481d6a
harris-miller <harrismillerconsulting@gmail.com>
2023-07-05 02:24:59 -0600

    more testing

--- test/path.test.ts
+++ test/path.test.ts
@@ -1,10 +1,12 @@
-import { expectType } from 'tsd';
+import { expectType, expectError } from 'tsd';
 
 import { path } from '../es';
 
-type Obj = { one: { two: { three: { four: { five: { six: { seven: { eight: { nine: boolean } } } } } } } } };
+type Obj = { one: { two: {
+  three: { four: { five: { six: { seven: { eight: { nine: boolean } } } } } },
+  other: boolean
+} } };
 
-// TODO
 expectType<Obj['one']>(path(['one'], {} as Obj));
 expectType<Obj['one']['two']>(path(['one', 'two'], {} as Obj));
 expectType<Obj['one']['two']['three']>(path(['one', 'two', 'three'], {} as Obj));
@@ -14,3 +16,8 @@ expectType<Obj['one']['two']['three']['four']['five']['six']>(path(['one', 'two'
 expectType<Obj['one']['two']['three']['four']['five']['six']['seven']>(path(['one', 'two', 'three', 'four', 'five', 'six', 'seven'], {} as Obj));
 expectType<Obj['one']['two']['three']['four']['five']['six']['seven']['eight']>(path(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'], {} as Obj));
 expectType<Obj['one']['two']['three']['four']['five']['six']['seven']['eight']['nine']>(path(['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], {} as Obj));
+
+// sibling keys
+expectType<boolean>(path(['one', 'two', 'other'], {} as Obj));
+// incorrect paths
+expectError(path(['one', 'two', 'nope'], {} as Obj));
