bfbe1fee85703a8196fc71573a7ee388278cb624
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 23:21:01 -0600

    best support IMHO

--- types/filter.d.ts
+++ types/filter.d.ts
@@ -1,25 +1,23 @@
-import { Placeholder } from './util/tools';
+import { Placeholder, ValueOfUnion } from './util/tools';
 
-// filter(pred)(collection)
-export function filter<T, P extends T>(
-  pred: (value: T) => value is P,
-): <C extends readonly T[] | Record<string, T>>(collection: C) => C extends readonly any[] ? P[] : Partial<Record<keyof C, P>>;
-export function filter<T>(
-  pred: (value: T) => boolean,
-): <C extends readonly T[] | Record<string, T>>(collection: C) => C extends readonly any[] ? T[] : Partial<C>;
+// filter(pred)(list)
+// there is purposefully no support for anything other than T[] here
+// See here: https://github.com/ramda/types/discussions/54
+export function filter<T, P extends T>(pred: (value: T) => value is P): (list: T[]) => P[];
+export function filter<T>(pred: (value: T) => boolean): (list: T[]) => T[];
 
 // filter(__, collection)(pred)
-export function filter<C>(__: Placeholder, dict: C): {
-  <T extends C[keyof C], P extends T>(pred: (val: T) => val is P): C extends readonly any[] ? P[] : Partial<Record<keyof C, P>>;
-  <T extends C[keyof C]>(pred: (val: T) => boolean): C extends readonly any[] ? T[] : Partial<Record<keyof C, T>>;
+export function filter<U>(__: Placeholder, dict: U): {
+  <T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P): Record<keyof U, P>;
+  <T extends ValueOfUnion<U>>(pred: (value: T) => boolean): U;
+};
+export function filter<T>(__: Placeholder, dict: T[]): {
+  <P extends T>(pred: (value: T) => value is P): P[];
+  (pred: (value: T) => boolean): T[];
 };
 
 // filter(pred, collection)
-export function filter<T, P extends T, C extends readonly T[] | Record<PropertyKey, T>>(
-  pred: (value: T) => value is P,
-  collection: C
-): C extends readonly T[] ? P[] : Partial<Record<keyof C, P>>;
-export function filter<T, C extends readonly T[] | Record<PropertyKey, T>>(
-  pred: (value: T) => boolean,
-  collection: C
-): C extends readonly any[] ? T[] : Partial<C>;
+export function filter<U, T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P, obj: U): Record<keyof U, P>;
+export function filter<T, P extends T>(pred: (value: T) => value is P, list: T[]): P[];
+export function filter<U, T extends ValueOfUnion<U>>(pred: (value: T) => boolean, obj: U): U;
+export function filter<T>(pred: (value: T) => boolean, list: T[]): T[];
