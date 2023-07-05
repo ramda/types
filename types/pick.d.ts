1380b1a592a128e80578216b1b5433ed3b924c59
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 18:58:27 -0600

    add const to Names

--- types/pick.d.ts
+++ types/pick.d.ts
@@ -1,4 +1,4 @@
 import { ElementOf } from './util/tools';
 
 export function pick<const Names extends readonly [PropertyKey, ...PropertyKey[]]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => string extends keyof U ? Record<string, U[keyof U]> : { [P in ElementOf<Names>]: U[P] };
-export function pick<U, Names extends readonly [keyof U, ...(keyof U)[]]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : { [P in ElementOf<Names>]: U[P] };
+export function pick<U, const Names extends readonly [keyof U, ...(keyof U)[]]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : { [P in ElementOf<Names>]: U[P] };
