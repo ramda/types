40e36deaeaa2a09449cfe946edd1f8572e56122a
harris-miller <harrismillerconsulting@gmail.com>
2023-07-04 23:35:50 -0600

    fixed up tests

--- test/filter.test.ts
+++ test/filter.test.ts
@@ -4,20 +4,19 @@ import { __, compose, filter, identity, isNotNil, pipe, map } from '../es';
 
 type StringOrUndefined = string | undefined;
 
-// when the predicate that is passed to filter has a generic argument, such as `isNotNil`
-// typescript collapses `T` to `unknown`
-expectAssignable<unknown[]>(filter(isNotNil)([] as StringOrUndefined[]));
-expectAssignable<unknown[]>(filter(isNotNil, [] as StringOrUndefined[]));
-expectAssignable<Record<string, unknown>>(filter(isNotNil)({} as Record<string, StringOrUndefined>));
-expectAssignable<Record<string, unknown>>(filter(isNotNil, {} as Record<string, StringOrUndefined>));
+// filter(pred)(filterable)
+// purposefully no support for object, see https://github.com/ramda/types/discussions/54
+expectType<string[]>(filter(isNotNil)([] as StringOrUndefined[]));
 
-// this is fixable by setting the generic on isNotNil
-expectType<string[]>(filter(isNotNil<StringOrUndefined>)([] as StringOrUndefined[]));
-expectType<string[]>(filter(isNotNil<StringOrUndefined>, [] as StringOrUndefined[]));
-expectType<Partial<Record<string, string>>>(filter(isNotNil<StringOrUndefined>)({} as Record<string, StringOrUndefined>));
+// filter(pred, filterable)
 expectType<Partial<Record<string, string>>>(filter(isNotNil<StringOrUndefined>, {} as Record<string, StringOrUndefined>));
-expectType<string[]>(filter(__, [] as StringOrUndefined[])(isNotNil<StringOrUndefined>));
-expectType<Partial<Record<string, string>>>(filter(__, {} as Record<string, StringOrUndefined>)(isNotNil<StringOrUndefined>));
+expectType<string[]>(filter(isNotNil, [] as StringOrUndefined[]));
+
+// filter(__, filterable)(pred)
+expectType<Partial<Record<string, string>>>(filter(__, {} as Record<string, StringOrUndefined>)(isNotNil));
+expectType<string[]>(filter(__, [] as StringOrUndefined[])(isNotNil));
+
+
 
 const gt5 = (num: number) => num > 5;
 const typed: number[] = [];
@@ -40,7 +39,7 @@ expectType<number[]>(filter(gt5)(readOnlyArr));
 expectType<number[]>(filter(gt5, [1, 4, 6, 10]));
 expectType<number[]>(filter(gt5)([1, 4, 6, 10]));
 // constArray
-expectType<number[]>(filter(gt5, constArray));
+expectType<(1 | 4 | 6 | 10)[]>(filter(gt5, constArray));
 expectType<number[]>(filter(gt5)(constArray));
 // tuple
 expectType<number[]>(filter(gt5, tuple));
@@ -65,16 +64,12 @@ const asConst = { foo: 4, bar: 6 } as const;
 
 // typed variables
 expectType<Partial<Obj>>(filter(gt5, typedO));
-expectType<Partial<Obj>>(filter(gt5)(typedO));
 // un-typed variable
 expectType<Partial<Obj>>(filter(gt5, inferredO));
-expectType<Partial<Obj>>(filter(gt5)(inferredO));
 // readonly
 expectType<Partial<{ readonly foo: 4, readonly bar: 6 }>>(filter(gt5, asConst));
-expectType<Partial<{ readonly foo: 4, readonly bar: 6 }>>(filter(gt5)(asConst));
 // literal
 expectType<Partial<Obj>>(filter(gt5, { foo: 4, bar: 6 }));
-expectType<Partial<Obj>>(filter(gt5)({ foo: 4, bar: 6 }));
 
 // pipe
 // the last overload is always selected when passing a function as an argument to a function like pipe
@@ -89,6 +84,6 @@ expectType<Partial<Record<keyof Obj, number>>>(filter(__, typedO)(gt5));
 // un-typed variable
 expectType<Partial<Obj>>(filter(__, inferredO)(gt5));
 // readonly
-expectType<Partial<Record<'foo' | 'bar', 4 | 6>>>(filter(__, asConst)(gt5));
+expectType<Partial<{ readonly foo: 4; readonly bar: 6; }>>(filter(__, asConst)(gt5));
 // literal
 expectType<Partial<Obj>>(filter(__, { foo: 4, bar: 6 })(gt5));
--- types/filter.d.ts
+++ types/filter.d.ts
@@ -3,21 +3,21 @@ import { Placeholder, ValueOfUnion } from './util/tools';
 // filter(pred)(list)
 // there is purposefully no support for anything other than T[] here
 // See here: https://github.com/ramda/types/discussions/54
-export function filter<T, P extends T>(pred: (value: T) => value is P): (list: T[]) => P[];
-export function filter<T>(pred: (value: T) => boolean): (list: T[]) => T[];
+export function filter<T, P extends T>(pred: (value: T) => value is P): (filterable: readonly T[]) => P[];
+export function filter<T>(pred: (value: T) => boolean): (filterable: readonly T[]) => T[];
 
 // filter(__, collection)(pred)
-export function filter<U>(__: Placeholder, dict: U): {
-  <T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P): Record<keyof U, P>;
-  <T extends ValueOfUnion<U>>(pred: (value: T) => boolean): U;
+export function filter<U>(__: Placeholder, filterable: U extends readonly any[] ? never : U): {
+  <T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P): Partial<Record<keyof U, P>>;
+  <T extends ValueOfUnion<U>>(pred: (value: T) => boolean): Partial<U>;
 };
-export function filter<T>(__: Placeholder, dict: T[]): {
+export function filter<T>(__: Placeholder, filterable: readonly T[]): {
   <P extends T>(pred: (value: T) => value is P): P[];
   (pred: (value: T) => boolean): T[];
 };
 
 // filter(pred, collection)
-export function filter<U, T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P, obj: U): Record<keyof U, P>;
-export function filter<T, P extends T>(pred: (value: T) => value is P, list: T[]): P[];
-export function filter<U, T extends ValueOfUnion<U>>(pred: (value: T) => boolean, obj: U): U;
-export function filter<T>(pred: (value: T) => boolean, list: T[]): T[];
+export function filter<U, T extends ValueOfUnion<U>, P extends T>(pred: (value: T) => value is P, filterable: U extends readonly any[] ? never : U): Partial<Record<keyof U, P>>;
+export function filter<T, P extends T>(pred: (value: T) => value is P, filterable: readonly T[]): P[];
+export function filter<U, T extends ValueOfUnion<U>>(pred: (value: T) => boolean, filterable: U extends readonly any[] ? never : U): Partial<U>;
+export function filter<T>(pred: (value: T) => boolean, filterable: readonly T[]): T[];
