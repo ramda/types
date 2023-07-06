6c5a627c2c518ff555c7eccaebbd658ed98cfa87
harris-miller <harrismillerconsulting@gmail.com>
2023-07-05 19:41:39 -0600

    correct overloaded return for modify(prop), not adding Placeholder versions yet

--- types/modify.d.ts
+++ types/modify.d.ts
@@ -1,5 +1,10 @@
-// modify(prop)(fn)(obj)
-export function modify<K extends PropertyKey>(prop: K):<T>(fn: (value: T) => T) => <U extends Record<K, T>>(object: U) => U;
+// modify(prop)
+export function modify<K extends PropertyKey>(prop: K): {
+  // modify(prop)(fn)(obj)
+  <T>(fn: (value: T) => T): <U extends Record<K, T>>(object: U) => U;
+  // modify(prop)(fn), obj)
+  <T, U extends Record<K, T>>(fn: (value: T) => T, object: U): U;
+};
 
 // modify(prop, fn)(obj)
 export function modify<K extends PropertyKey, T>(prop: K, fn: (value: T) => T): <U extends Record<K, T>>(object: U) => U;
