fede08f58438de37a5b29a7f9f38a23df88f35da
harris-miller <harrismillerconsulting@gmail.com>
2023-07-06 02:19:24 -0600

    use native Pick utility type function

--- types/pick.d.ts
+++ types/pick.d.ts
@@ -1,4 +1,5 @@
 import { ElementOf } from './util/tools';
 
-export function pick<const Names extends readonly [PropertyKey, ...PropertyKey[]]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => string extends keyof U ? Record<string, U[keyof U]> : { [P in ElementOf<Names>]: U[P] };
-export function pick<U, const Names extends readonly [keyof U, ...(keyof U)[]]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : { [P in ElementOf<Names>]: U[P] };
+export function pick<const Names extends readonly [PropertyKey, ...PropertyKey[]]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
+export function pick<U, const Names extends readonly [keyof U, ...(keyof U)[]]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
+
