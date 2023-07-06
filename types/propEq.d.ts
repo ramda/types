31dc92ab9fe3a14c9df3c5afc4db5877fd5e93f6
harris-miller <harrismillerconsulting@gmail.com>
2023-07-06 02:10:13 -0600

    propEq tests

--- types/propEq.d.ts
+++ types/propEq.d.ts
@@ -1,8 +1,6 @@
-export function propEq<K extends string | number>(
-  val: any,
-): {
-  (name: K): (obj: Record<K, any>) => boolean;
-  (name: K, obj: Record<K, any>): boolean;
+export function propEq<T>(val: T): {
+  <K extends PropertyKey>(name: K): (obj: Record<K, T>) => boolean;
+  <K extends PropertyKey>(name: K, obj: Record<K, T>): boolean;
 };
-export function propEq<K extends string | number>(val: any, name: K): (obj: Record<K, any>) => boolean;
-export function propEq<K extends string | number>(val: any, name: K, obj: Record<K, any>): boolean;
+export function propEq<T, K extends PropertyKey>(val: T, name: K): (obj:  Record<K, T>) => boolean;
+export function propEq<K extends keyof U, U>(val: U[K], name: K, obj: U): boolean;
