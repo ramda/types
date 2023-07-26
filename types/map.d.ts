a5b88414d22a928b2682603c701ac84b45aabd98
harris-miller <harrismillerconsulting@gmail.com>
2023-07-25 21:13:17 -0600

    map support too

--- types/map.d.ts
+++ types/map.d.ts
@@ -1,28 +1,23 @@
 import { FunctorMap, FunctorFantasyLand, Placeholder, ValueOfUnion } from './util/tools';
 
 // map(fn)
-export function map<A, B>(fn: (x: A) => B): {
-  // first and last def are the same and are here on purpose
-  // the list variant needs to come before the FunctorMap ones, because `T[]` is a `FunctorMap<T>`
-  (list: readonly A[]): B[];
-  (functor: FunctorFantasyLand<A>): FunctorFantasyLand<B>;
-  (functor: FunctorMap<A>): FunctorMap<B>;
-  <U extends Record<PropertyKey, A>>(dict: U): Record<keyof U, B>;
-  // it also needs to be here when you pass map as an argument to a function, eg `compose(map(fn))`
-  (list: readonly A[]): B[];
-};
+// there is purposefully no support for anything other than T[] here
+// See here: https://github.com/ramda/types/discussions/54
+export function map<A, B>(fn: (value: A) => B): (list: readonly A[]) => B[];
 
 // map(__, list)
-export function map<A>(__: Placeholder, list: readonly A[]): <B>(fn: (x: A) => B) => B[];
-export function map<A>(__: Placeholder, obj: FunctorFantasyLand<A>): <B>(fn: (a: A) => B) => FunctorFantasyLand<B>;
-export function map<A>(__: Placeholder, obj: FunctorMap<A>): <B>(fn: (a: A) => B) => FunctorMap<B>;
-export function map<U extends object>(__: Placeholder, dict: U): <B>(fn: (x: ValueOfUnion<B>) => B) => Record<keyof U, B>;
+// the list variant needs to come before the FunctorMap ones, because `T[]` is a `FunctorMap<T>`
+export function map<A>(__: Placeholder, list: readonly A[]): <B>(fn: (value: A) => B) => B[];
+export function map<A>(__: Placeholder, obj: FunctorFantasyLand<A>): <B>(fn: (value: A) => B) => FunctorFantasyLand<B>;
+export function map<A>(__: Placeholder, obj: FunctorMap<A>): <B>(fn: (value: A) => B) => FunctorMap<B>;
+export function map<U extends object>(__: Placeholder, dict: U): <B>(fn: (value: ValueOfUnion<B>) => B) => Record<keyof U, B>;
+
 // map(fn, list)
 // first and last def are the same and are here on purpose
 // the list variant needs to come before the FunctorMap ones, because `T[]` is a `FunctorMap<T>`
-export function map<A, B>(fn: (x: A) => B, list: readonly A[]): B[];
-export function map<A, B>(fn: (x: A) => B, obj: FunctorFantasyLand<A>): FunctorFantasyLand<B>;
-export function map<A, B>(fn: (x: A) => B, obj: FunctorMap<A>): FunctorMap<B>;
+export function map<A, B>(fn: (value: A) => B, list: readonly A[]): B[];
+export function map<A, B>(fn: (value: A) => B, obj: FunctorFantasyLand<A>): FunctorFantasyLand<B>;
+export function map<A, B>(fn: (value: A) => B, obj: FunctorMap<A>): FunctorMap<B>;
 export function map<U extends object, B>(fn: (x: ValueOfUnion<U>) => B, dict: U): Record<keyof U, B>;
 // it also needs to be here when you pass map as an argument to a function, eg `flip(map)`
-export function map<A, B>(fn: (x: A) => B, list: readonly A[]): B[];
+export function map<A, B>(fn: (value: A) => B, list: readonly A[]): B[];
