3f8e31d884a1630f04eebb813f279c1bc3671c24
Harris Miller <harrismillerconsulting@gmail.com>
2023-10-02 00:16:07 -0600

    Update: Pick (#52)

---
+++ test/pick.test.ts
@@ -0,0 +1,24 @@
+import { expectType, expectError } from 'tsd';
+
+import { pick, KeysAsTuple } from '../es';
+
+const obj = { foo: 1, bar: '2', biz: false };
+
+expectType<{ foo: number; }>(pick(['foo'])(obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'])(obj));
+expectType<{ foo: number; bar: string; biz: boolean }>(pick(['foo', 'bar', 'biz'])(obj));
+expectError(pick(['baz', 'bar', 'biz'])(obj));
+
+expectType<{ foo: number; }>(pick(['foo'], obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'], obj));
+expectType<{ foo: number; bar: string; biz: boolean }>(pick(['foo', 'bar', 'biz'], obj));
+expectError(pick(['baz', 'bar', 'biz'], obj));
+
+// Record
+expectType<Record<string, number>>(pick(['foo', 'bar'], {} as Record<string, number>));
+
+const names: string[] = ['foo', 'bar'];
+// in cases where names is either `string[]` or `(keyof obj)[]`, cast with supplied `KeysAsTuple` type function
+expectType<typeof obj>(pick(names as KeysAsTuple<typeof obj>, obj));
+// this case however is inaccurate, best to cast as a `Partial` in a real-world scenario
+expectType<Partial<typeof obj>>(pick(names as KeysAsTuple<typeof obj>, obj) as Partial<typeof obj>);
--- types/util/tools.d.ts
+++ types/util/tools.d.ts
@@ -500,3 +500,18 @@ export type WidenLiterals<T> =
       : T extends number
         ? number
         : T;
+
+/**
+ * Extract the types from an array
+ * Works with Tuples, eg `ElementOf<typeof ['p1', 'p2']>` => `'p1' | 'p2'`
+ *
+ * <created by @harris-miller>
+ */
+export type ElementOf<Type extends readonly any[]> = Type extends readonly (infer Values)[] ? Values : never;
+
+/**
+ * Convenance type function to extract keys of an object as a tuple literal
+ *
+ * <created by @harris-miller>
+ */
+export type KeysAsTuple<T> = [keyof T, ...(keyof T)[]];
