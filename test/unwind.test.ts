09025dc282152b36ea39ae8a2ab7084a0f303add
harris-miller <harrismillerconsulting@gmail.com>
2023-10-02 00:31:17 -0600

    removed unknown key support and added tests for Record support

--- test/unwind.test.ts
+++ test/unwind.test.ts
@@ -1,4 +1,4 @@
-import { expectType } from 'tsd';
+import { expectType, expectError } from 'tsd';
 import { unwind } from '../types/unwind';
 import { __ } from '../types/__';
 
@@ -6,14 +6,38 @@ type Obj = { name: string; hobbies: string[]; colors: string[] };
 
 const obj: Obj = { name: 'alice', hobbies: ['Golf', 'Hacking'], colors: ['red', 'green'] };
 
+// unwind(key, obj)
 expectType<Array<Omit<Obj, 'hobbies'> & Record<'hobbies', string>>>(unwind('hobbies', obj));
 expectType<[Obj]>(unwind('name', obj));
-expectType<[Obj]>(unwind('foobar', obj));
+expectError(unwind('foobar', obj));
 
+// unwind(key)(obj)
 expectType<Array<Omit<Obj, 'hobbies'> & Record<'hobbies', string>>>(unwind('hobbies')(obj));
 expectType<[Obj]>(unwind('name')(obj));
-expectType<[Obj]>(unwind('foobar')(obj));
+expectError(unwind('foobar')(obj));
 
+// unwind(__, obj)(key)
 expectType<Array<Omit<Obj, 'hobbies'> & Record<'hobbies', string>>>(unwind(__, obj)('hobbies'));
 expectType<[Obj]>(unwind(__, obj)('name'));
-expectType<[Obj]>(unwind(__, obj)('foobar'));
+expectError(unwind(__, obj)('foobar'));
+
+// Records
+// when value is `T[]`
+expectType<Array<Omit<Record<string, string[]>, 'hobbies'> & Record<'hobbies', string>>>(unwind('hobbies', {} as Record<string, string[]>));
+expectType<Array<Omit<Record<string, number[]>, 'hobbies'> & Record<'hobbies', number>>>(unwind('hobbies', {} as Record<string, number[]>));
+
+expectType<Array<Omit<Record<string, string[]>, 'hobbies'> & Record<'hobbies', string>>>(unwind('hobbies')({} as Record<string, string[]>));
+expectType<Array<Omit<Record<string, number[]>, 'hobbies'> & Record<'hobbies', number>>>(unwind('hobbies')({} as Record<string, number[]>));
+
+expectType<Array<Omit<Record<string, string[]>, 'hobbies'> & Record<'hobbies', string>>>(unwind(__, {} as Record<string, string[]>)('hobbies'));
+expectType<Array<Omit<Record<string, number[]>, 'hobbies'> & Record<'hobbies', number>>>(unwind(__, {} as Record<string, number[]>)('hobbies'));
+
+// when value is `T`, not an array
+expectType<[Record<string, string>]>(unwind('hobbies', {} as Record<string, string>));
+expectType<[Record<string, number>]>(unwind('hobbies', {} as Record<string, number>));
+
+expectType<[Record<string, string>]>(unwind('hobbies')({} as Record<string, string>));
+expectType<[Record<string, number>]>(unwind('hobbies')({} as Record<string, number>));
+
+expectType<[Record<string, string>]>(unwind(__, {} as Record<string, string>)('hobbies'));
+expectType<[Record<string, number>]>(unwind(__, {} as Record<string, number>)('hobbies'));
--- types/unwind.d.ts
+++ types/unwind.d.ts
@@ -5,11 +5,8 @@ import { Placeholder } from './util/tools';
 // if K does not exist on O, [O] is returned
 export function unwind<O extends object>(__: Placeholder, obj: O): {
   <K extends keyof O>(key: K): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
-  <K extends PropertyKey>(key: K): [O];
 };
 export function unwind<K extends keyof O, O>(key: K, obj: O): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
-export function unwind<K extends PropertyKey, O>(key: K, obj: O): [O];
 export function unwind<K extends PropertyKey>(key: K): {
   <O extends Record<K, any>>(obj: O): O[K] extends Array<infer T> ? Array<Omit<O, K> & Record<K, T>> : [O];
-  <O>(obj: O): [O];
 };
