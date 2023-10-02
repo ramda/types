426c0ad4f337059b5384b70e0048787852a3e7aa
Harris Miller <harrismillerconsulting@gmail.com>
2023-10-02 13:29:26 -0600

    Update: Pluck (#61)

--- types/pluck.d.ts
+++ types/pluck.d.ts
@@ -1,4 +1,10 @@
-export function pluck<P extends string>(p: P): <T>(list: Array<Record<P, T>>) => T[];
-export function pluck(p: number): <T>(list: Array<{ [k: number]: T }>) => T[];
-export function pluck<K extends keyof T, T>(p: K, list: readonly T[]): Array<T[K]>;
-export function pluck<T>(p: number, list: Array<{ [k: number]: T }>): T[];
+import { Placeholder } from 'ramda';
+
+export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
+  <U extends readonly unknown[] | Record<K, any>>(obj: Record<PropertyKey, U>): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
+  <U extends readonly unknown[] | Record<K, any>>(list: U[]): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
+};
+export function pluck<U>(__: Placeholder, obj: Record<PropertyKey, U>): <K extends keyof U>(prop: K) => Array<U[K]>;
+export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
+export function pluck<K extends keyof U, U>(prop: K, obj: Record<PropertyKey, U>): Array<U[K]>;
+export function pluck<K extends keyof U, U>(prop: K, list: readonly U[]): Array<U[K]>;
