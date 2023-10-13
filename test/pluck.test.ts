13834ee69bfa79019b8eda17ab302848d9180e3a
Kharazyan Aram <armxaz@gmail.com>
2023-10-13 11:52:44 +0400

    fix(pluck): pluck now can return object

--- test/pluck.test.ts
+++ test/pluck.test.ts
@@ -4,6 +4,16 @@ import { __, pluck } from '../es';
 
 type Obj = { name: string; age: number };
 
+const record = {
+  a: {name: 'foo', age: 13},
+  b: {name: 'bar', age: 31, desc: 'barrr'}
+};
+const incorrectRecord = {
+  a: {name: 'foo', age: 13},
+  b: {name: 'bar', age: 31, desc: 'barrr'},
+  c: {}
+};
+
 // pluck(key, list)
 expectType<string[]>(pluck('name', [] as Obj[]));
 expectType<number[]>(pluck('age', [] as Obj[]));
@@ -20,16 +30,19 @@ expectType<number[]>(pluck(__, [] as Obj[])('age'));
 expectError(pluck(__, [] as Obj[])('nope'));
 
 // pluck(key, record)
-expectType<string[]>(pluck('name', {} as Record<string, Obj>));
-expectType<number[]>(pluck('age', {} as Record<string, Obj>));
-expectError(pluck('nope', {} as Record<string, Obj>));
+expectType<{ a: string, b: string }>(pluck('name', record));
+expectType<{ a: number, b: number }>(pluck('age', record));
+expectError(pluck('nope', record));
+expectError(pluck('age', incorrectRecord));
 
 // pluck(key)(record)
-expectType<string[]>(pluck('name')({} as Record<string, Obj>));
-expectType<number[]>(pluck('age')({} as Record<string, Obj>));
-expectError(pluck('nope')({} as Record<string, Obj>));
+expectType<{ a: string, b: string }>(pluck('name')(record));
+expectType<{ a: number, b: number }>(pluck('age')(record));
+expectError(pluck('nope')(record));
+expectError(pluck('age')(incorrectRecord));
 
 // pluck(__, record)(prop)
-expectType<string[]>(pluck(__, {} as Record<string, Obj>)('name'));
-expectType<number[]>(pluck(__, {} as Record<string, Obj>)('age'));
-expectError(pluck(__, {} as Record<string, Obj>)('nope'));
+expectType<{ a: string, b: string }>(pluck(__, record)('name'));
+expectType<{ a: number, b: number }>(pluck(__, record)('age'));
+expectError(pluck(__, record)('nope'));
+expectError(pluck(__, incorrectRecord)('age'));
--- types/pluck.d.ts
+++ types/pluck.d.ts
@@ -1,10 +1,10 @@
 import { Placeholder } from 'ramda';
 
 export function pluck<K extends PropertyKey>(prop: K extends Placeholder ? never : K): {
-  <U extends readonly unknown[] | Record<K, any>>(obj: Record<PropertyKey, U>): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
+  <U extends Record<K, any>, IK extends string>(obj: Record<IK, U>): { [KK in keyof typeof obj]: U[K] };
   <U extends readonly unknown[] | Record<K, any>>(list: U[]): U extends readonly (infer T)[] ? T[] : U extends Record<K, infer T> ? T[] : never;
 };
-export function pluck<U>(__: Placeholder, obj: Record<PropertyKey, U>): <K extends keyof U>(prop: K) => Array<U[K]>;
-export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
-export function pluck<K extends keyof U, U>(prop: K, obj: Record<PropertyKey, U>): Array<U[K]>;
+export function pluck<K extends keyof U, U extends Record<any, any>, IK extends keyof any>(prop: K, record: Record<IK, U>): { [KK in keyof typeof record]: U[K] };
 export function pluck<K extends keyof U, U>(prop: K, list: readonly U[]): Array<U[K]>;
+export function pluck<U extends Record<any, any>, IK extends keyof any>(__: Placeholder, record: Record<IK, U>): <K extends keyof U>(prop: K) => { [KK in keyof typeof record]: U[K] };
+export function pluck<U>(__: Placeholder, list: readonly U[]): <K extends keyof U>(prop: K) => Array<U[K]>;
