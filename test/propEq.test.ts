0e662108353c591fc3ea29b23958fb0df1f122a2
harris-miller <harrismillerconsulting@gmail.com>
2023-10-15 12:26:51 -0600

    mimic how comparitors work in typescript

--- test/propEq.test.ts
+++ test/propEq.test.ts
@@ -10,17 +10,31 @@ type Obj = {
   n: null;
 };
 
+// explanation
+// `obj.union` is type `'A' | 'B'` and `val` is `string`
+// typescript allows comparison because as long as one side extends the other, it's ok
+function doesEq(val: string, obj: Obj) {
+  return obj.union === val;
+}
+// this is different from assignment that errors because`string` is too wide for `'A' | 'B'`
+function assign(val: string, obj: Obj) {
+  // @ts-expect-error -- remove this to see the error (need this so `npm run test` pasts)
+  obj.union = val;
+}
+
+// this is why we use `WidenLiterals` in the type definition
+
 // propEq(val, name, obj)
 expectType<boolean>(propEq('foo', 'union', {} as Obj));
-// non-union string fails
-expectError(propEq('nope', 'union', {} as Obj));
+// any string works here, see the above explanation as to why
+expectType<boolean>(propEq('else', 'union', {} as Obj));
 // completely different type fails
 expectError(propEq(2, 'union', {} as Obj));
 
 // propEq(val)(name)(obj)
 expectType<boolean>(propEq('foo')('union')({} as Obj));
-// 'nope' is inferred as 'string' here.
-expectType<boolean>(propEq('nope')('union')({} as Obj));
+// any string works here, see the above explanation as to why
+expectType<boolean>(propEq('else')('union')({} as Obj));
 // completely different type fails
 expectError(propEq(2)('union')({} as Obj));
 
@@ -33,7 +47,7 @@ expectError(propEq(2)('union', {} as Obj));
 
 // propEq(val, name)(obj)
 expectType<boolean>(propEq('foo', 'union')({} as Obj));
-// 'nope' is inferred as 'string' here.
-expectType<boolean>(propEq('nope', 'union')({} as Obj));
+// any string works here, see the above explanation as to why
+expectType<boolean>(propEq('else', 'union')({} as Obj));
 // completely different type fails
 expectError(propEq(2, 'union')({} as Obj));
