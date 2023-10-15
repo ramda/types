87420b6f574e5be50567f39075dc9fb25679539c
harris-miller <harrismillerconsulting@gmail.com>
2023-10-15 17:11:55 -0600

    back this up, lets do this a bit at a time

--- types/propEq.d.ts
+++ types/propEq.d.ts
@@ -1,40 +1,14 @@
-import { Placeholder, WidenLiterals } from './util/tools';
+import { WidenLiterals } from './util/tools';
 
 // propEq(val)
 export function propEq<T>(val: T): {
   // propEq(val)(name)(obj)
-  <K extends PropertyKey>(name: K): (obj: Record<K, WidenLiterals<T>>) => boolean;
-  // propEq(val)(__, obj)(name)
-  <U>(__: Placeholder, obj: U): <K extends keyof U>(name: K) => U[K] extends WidenLiterals<T> ? boolean : never;
+  <K extends PropertyKey>(name: K): <U extends Partial<Record<K, any>>>(obj: U) => T extends WidenLiterals<U[K]> ? boolean : never;
   // propEq(val)(name, obj)
-  <K extends PropertyKey>(name: K, obj: Record<K, WidenLiterals<T>>): boolean;
-};
-
-// propEq(__, name)
-export function propEq<K extends PropertyKey>(__: Placeholder, name: K): {
-  // propEq(val)(obj)
-  <T>(val: T): (obj: Record<K, WidenLiterals<T>>) => boolean;
-  // propEq(__, obj)(val)
-  <U extends Record<K, any>>(__: Placeholder, obj: U): (val: WidenLiterals<U[K]>) => boolean;
-  // propEq(val, obj)
-  <T>(val: T, obj: Record<K, WidenLiterals<T>>): boolean;
+  // type it this way for better error message for unknown keys
+  <K extends keyof U, U extends Partial<Record<PropertyKey, any>>>(name: K, obj: U): T extends WidenLiterals<U[K]> ? boolean : never;
 };
 // propEq(val, name)(obj)
-export function propEq<T, K extends PropertyKey>(val: T, name: K): (obj: Record<K, WidenLiterals<T>>) => boolean;
-
-// propEq(__, __, obj)
-export function propEq<U>(__: Placeholder, __2: Placeholder, obj: U): {
-  // propEq(__, __, obj)(val)(name)
-  <T>(val: T): <K extends keyof U>(name: K) => boolean;
-  // propEq(__, __, obj)(__, key)(name)
-  <K extends keyof U>(__: Placeholder, key: K): (val: WidenLiterals<U[K]>) => boolean;
-  // propEq(__, __, obj)(name, key)
-  <K extends keyof U>(val: WidenLiterals<U[K]>, name: K): boolean;
-};
-
-// propEq(__, name, obj)(val)
-export function propEq<K extends keyof U, U>(__: Placeholder, name: K, obj: U): (val: WidenLiterals<U[K]>) => boolean;
-// propEq(val, __, obj)(name)
-export function propEq<T, U>(val: T, __: Placeholder, obj: U): <K extends keyof U>(name: K) => U[K] extends WidenLiterals<T> ? boolean : never;
+export function propEq<T, K extends PropertyKey>(val: T, name: K): <U extends Partial<Record<PropertyKey, any>>>(obj: U) => T extends WidenLiterals<U[K]> ? boolean : never;
 // propEq(val, name, obj)
-export function propEq<K extends keyof U, U>(val: WidenLiterals<U[K]>, name: K, obj: U): boolean;
+export function propEq<K extends keyof U, U>(val: U[K], name: K, obj: U): boolean;
