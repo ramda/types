48f4655595b414cb1010bb9754630f80f34985b7
harris-miller <harrismillerconsulting@gmail.com>
2023-11-08 21:31:22 -0700

    working on a ts4 support version

---
+++ types-ts4/pick.d.ts
@@ -0,0 +1,4 @@
+import { ElementOf } from './util/tools';
+
+export function pick<Names extends readonly PropertyKey[]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
+export function pick<U, Names extends readonly (keyof U)[]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
