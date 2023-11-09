9bc543fbf37751d87303832703ab7b841b6df055
harris-miller <harrismillerconsulting@gmail.com>
2023-11-08 21:29:47 -0700

    lint fix

--- types/pick.d.ts
+++ types/pick.d.ts
@@ -2,4 +2,3 @@ import { ElementOf } from './util/tools';
 
 export function pick<const Names extends readonly PropertyKey[]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
 export function pick<U, const Names extends readonly (keyof U)[]>(names: Names, obj: U): string extends keyof U ? Record<string, U[keyof U]> : Pick<U, ElementOf<Names>>;
-
