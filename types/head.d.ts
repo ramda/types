a2a644f922e104c2f264ed724e61797aef7f1d5a
harris-miller <harrismillerconsulting@gmail.com>
2023-11-15 23:25:06 -0700

    WIP

--- types/head.d.ts
+++ types/head.d.ts
@@ -1,8 +1,10 @@
+// empty tuple - purposefully `never`. `head` should never work on an empty string
+export function head(str: ''): never;
 // string
 export function head(str: string): string;
 // empty tuple - purposefully `never`. `head` should never work on tuple type with no length
 export function head(list: readonly []): never;
-// non-empty tuple
+// non-empty tuple - this can be a [T1, T2, T3] style tuple or a `NonEmptyArray` type that can be produced by `isNotEmpty`
 export function head<T1, TRest>(list: readonly [T1, ...TRest[]]): T1;
 // arrays, because these could be empty, they return `T | undefined`
 // this is no different than the tuple form since `T[]` can be empty at runtime
--- types/isNotEmpty.d.ts
+++ types/isNotEmpty.d.ts
@@ -1,5 +1,5 @@
-import { NonEmptyArray, ReadOnlyNonEmptyArray } from './util/tools';
+import { NonEmptyArray, ReadonlyNonEmptyArray } from './util/tools';
 
-export function isNotEmpty<T>(value: readonly T[]): value is ReadOnlyNonEmptyArray<T>;
+export function isNotEmpty<T>(value: readonly T[]): value is ReadonlyNonEmptyArray<T>;
 export function isNotEmpty<T>(value: T[]): value is NonEmptyArray<T>;
 export function isNotEmpty(value: any): boolean;
