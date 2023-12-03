d77a565d547fac476bd7d3e327ab46c55b8f01fd
Lautaro Dragan <lautaro.dragan@gmail.com>
2023-12-03 01:11:49 -0300

    stricter omit

--- types/omit.d.ts
+++ types/omit.d.ts
@@ -1,2 +1,2 @@
-export function omit<K extends string>(names: readonly K[]): <T>(obj: T) => Omit<T, K>;
-export function omit<T, K extends string>(names: readonly K[], obj: T): Omit<T, K>;
+export function omit<K extends string>(names: readonly K[]): <T extends { [Property in K]: any }>(obj: T) => Omit<T, K>;
+export function omit<T, K extends keyof T>(names: K[], obj: T): Omit<T, K>;
