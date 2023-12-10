a1c3b85ed100e75f89d6ca98bb1f0e4f10be8438
Lautaro Dragan <lautaro.dragan@gmail.com>
2023-12-10 01:44:16 -0300

    partial

--- types/omit.d.ts
+++ types/omit.d.ts
@@ -1,4 +1,4 @@
 import { ElementOf } from './util/tools';
 
-export function omit<const Names extends readonly string[]>(names: Names): <U extends Record<ElementOf<Names>, any>>(obj: U) => Omit<U, ElementOf<Names>>;
+export function omit<const Names extends readonly string[]>(names: Names): <U extends Partial<Record<ElementOf<Names>, any>>>(obj: U) => Omit<U, ElementOf<Names>>;
 export function omit<U, const Names extends readonly (keyof U)[]>(names: Names, obj: U): Omit<U, ElementOf<Names>>;
