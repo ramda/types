c99ea972fc8a81fd47ff528f1cb813e5c7601c2c
harris-miller <harrismillerconsulting@gmail.com>
2024-01-03 05:24:26 -0700

    added some curry support

--- types/modify.d.ts
+++ types/modify.d.ts
@@ -1,3 +1,14 @@
+// modify(prop)
+export function modify<K extends PropertyKey>(prop: K): {
+  // modify(prop)(fn)(target)
+  <A>(fn: (a: A) => A): <U extends Record<K, A>>(target: U) => U;
+  <A, B>(fn: (a: A) => B): <U extends Record<K, A>>(target: U) => Omit<U, K> & Record<K, B>;
+  // modify(prop)(fn, target)
+  <A, U extends Record<K, A>>(fn: (a: A) => A, target: U): U;
+  <A, B, U extends Record<K, A>>(fn: (a: A) => B, target: U): Omit<U, K> & Record<K, B>;
+};
+
+// modify (prop, fn)(target)
 export function modify<K extends PropertyKey, A>(
   prop: K,
   fn: (a: A) => A,
@@ -7,6 +18,7 @@ export function modify<K extends PropertyKey, A, B>(
   fn: (a: A) => B,
 ): <U extends Record<K, A>>(target: U) => Omit<U, K> & Record<K, B>;
 
+// modify (prop, fn, target)
 export function modify<U extends object, K extends keyof U>(
   prop: K,
   fn: (a: U[K]) => U[K],
