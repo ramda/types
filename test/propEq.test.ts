806ea43bcc0b6caca9cd4f5efa30379b889feccd
harris-miller <harrismillerconsulting@gmail.com>
2024-01-03 00:12:24 -0700

    big big update

--- test/propEq.test.ts
+++ test/propEq.test.ts
@@ -4,85 +4,162 @@ import { propEq } from '../es';
 
 type Obj = {
   literals: 'A' | 'B';
-  union: number | string;
-  nullable: number | null;
+  unions: number | string;
+  nullable: number | null | undefined;
   optional?: number;
 };
 
 const obj = {} as Obj;
 
-// propEq(val)(name)(obj)
+const literalVar = 'A';
+let typedVar: 'A' | 'B' = 'A';
+
+//
+// literals
+//
+
+// happy path works as expected
 expectType<boolean>(propEq('A')('literals')(obj));
-// any string works here and will return boolean, we can live with that
-expectType<boolean>(propEq('C')('literals')(obj));
-// a different base type returns `never`
-expectType<never>(propEq(2)('literals')(obj));
-// unions work
-expectType<boolean>(propEq(2)('union')(obj));
-expectType<boolean>(propEq('2')('union')(obj));
-expectType<never>(propEq(true)('union')(obj));
-// nullable works
-expectType<boolean>(propEq(2)('nullable')(obj));
-expectType<boolean>(propEq(null)('nullable')(obj));
-// optionals work
-expectType<boolean>(propEq(2)('optional')(obj));
-expectType<boolean>(propEq(undefined)('optional')(obj));
-// unknownKey errors
-expectError(propEq('whatever')('unknownKey')(obj));
-
-// propEq(val)(name, obj)
-expectType<boolean>(propEq('A')('literals', obj));
-// any string works here and will return boolean, we can live with that
-expectType<boolean>(propEq('C')('literals', obj));
-// a different base type returns `never`
-expectType<never>(propEq(2)('literals', obj));
-// unions work
-expectType<boolean>(propEq(2)('union', obj));
-expectType<boolean>(propEq('2')('union', obj));
-expectType<never>(propEq(true)('union', obj));
-// nullable works
-expectType<boolean>(propEq(2)('nullable', obj));
-expectType<boolean>(propEq(null)('nullable', obj));
-// optionals work
-expectType<boolean>(propEq(2)('optional', obj));
-expectType<boolean>(propEq(undefined)('optional', obj));
-// unknownKey errors
-expectError(propEq('whatever')('unknownKey', obj));
-
-// propEq(val, name)(obj)
 expectType<boolean>(propEq('A', 'literals')(obj));
-// any string works here and will return boolean, we can live with that
-expectType<boolean>(propEq('C', 'literals')(obj));
-// a different base type returns `never`
-expectType<never>(propEq(2, 'literals')(obj));
-// unions work
-expectType<boolean>(propEq(2, 'union')(obj));
-expectType<boolean>(propEq('2', 'union')(obj));
-expectType<never>(propEq(true, 'union')(obj));
-// nullable works
-expectType<boolean>(propEq(2, 'nullable')(obj));
-expectType<boolean>(propEq(null, 'nullable')(obj));
-// optionals work
-expectType<boolean>(propEq(2, 'optional')(obj));
-expectType<boolean>(propEq(undefined, 'optional')(obj));
-// unknownKey errors
-expectError(propEq('whatever', 'unknownKey')(obj));
-
-// propEq(val, name, obj)
 expectType<boolean>(propEq('A', 'literals', obj));
-// any string works here and will return boolean, we can live with that
+
+// rejects if typeof val not U[K]
+expectError(propEq('C')('literals')(obj));
+expectError(propEq('C', 'literals')(obj));
 expectError(propEq('C', 'literals', obj));
-// a different base type returns `never`
+
+expectError(propEq(2)('literals')(obj));
+expectError(propEq(2, 'literals')(obj));
 expectError(propEq(2, 'literals', obj));
-// unions work
-expectType<boolean>(propEq(2, 'union', obj));
-expectType<boolean>(propEq('2', 'union', obj));
-expectError(propEq(true, 'union', obj));
-// nullable works
-expectType<boolean>(propEq(2, 'nullable', obj));
+
+// works for variable literal of correct type
+expectType<boolean>(propEq(literalVar)('literals')(obj));
+expectType<boolean>(propEq(literalVar, 'literals')(obj));
+expectType<boolean>(propEq(literalVar, 'literals', obj));
+
+// works for variable typed to be same
+expectType<boolean>(propEq(typedVar)('literals')(obj));
+expectType<boolean>(propEq(typedVar, 'literals')(obj));
+expectType<boolean>(propEq(typedVar, 'literals', obj));
+
+// rejects if typeof val is too wide
+expectError(propEq('A' as string)('literals')(obj));
+expectError(propEq('A' as string, 'literals')(obj));
+expectError(propEq('A' as string, 'literals', obj));
+
+// rejects if key is not on obj
+expectError(propEq('A')('literals')({} as Omit<Obj, 'literals'>));
+expectError(propEq('A', 'literals')({} as Omit<Obj, 'literals'>));
+expectError(propEq('A', 'literals', {} as Omit<Obj, 'literals'>));
+
+// rejects empty object literal
+expectError(propEq('A')('literals')({}));
+expectError(propEq('A', 'literals')({}));
+expectError(propEq('A', 'literals', {}));
+
+//
+// unions
+//
+
+// happy path works as expected
+expectType<boolean>(propEq('1')('unions')(obj));
+expectType<boolean>(propEq('1', 'unions')(obj));
+expectType<boolean>(propEq('1', 'unions', obj));
+
+expectType<boolean>(propEq(1)('unions')(obj));
+expectType<boolean>(propEq(1, 'unions')(obj));
+expectType<boolean>(propEq(1, 'unions', obj));
+
+// rejects if typeof val not part of union type
+expectError(propEq(true)('unions')(obj));
+expectError(propEq(true, 'unions')(obj));
+expectError(propEq(true, 'unions', obj));
+
+// rejects if key is not on obj
+expectError(propEq('1')('unions')({} as Omit<Obj, 'unions'>));
+expectError(propEq('1', 'unions')({} as Omit<Obj, 'unions'>));
+expectError(propEq('1', 'unions', {} as Omit<Obj, 'unions'>));
+
+// rejects empty object literal
+expectError(propEq('1')('unions')({}));
+expectError(propEq('1', 'unions')({}));
+expectError(propEq('1', 'unions', {}));
+
+//
+// nullable
+//
+
+// happy path works as expected
+expectType<boolean>(propEq(1)('nullable')(obj));
+expectType<boolean>(propEq(1, 'nullable')(obj));
+expectType<boolean>(propEq(1, 'nullable', obj));
+
+expectType<boolean>(propEq(null)('nullable')(obj));
+expectType<boolean>(propEq(null, 'nullable')(obj));
 expectType<boolean>(propEq(null, 'nullable', obj));
-// optionals work
-expectType<boolean>(propEq(2, 'optional', obj));
+
+expectType<boolean>(propEq(undefined)('nullable')(obj));
+expectType<boolean>(propEq(undefined, 'nullable')(obj));
+expectType<boolean>(propEq(undefined, 'nullable', obj));
+
+// rejects if typeof val not part of union type
+expectError(propEq(true)('nullable')(obj));
+expectError(propEq(true, 'nullable')(obj));
+expectError(propEq(true, 'nullable', obj));
+
+// rejects if key is not on obj
+expectError(propEq(1)('nullable')({} as Omit<Obj, 'nullable'>));
+expectError(propEq(1, 'nullable')({} as Omit<Obj, 'nullable'>));
+expectError(propEq(1, 'nullable', {} as Omit<Obj, 'nullable'>));
+
+// rejects empty object literal
+expectError(propEq(1)('nullable')({}));
+expectError(propEq(1, 'nullable')({}));
+expectError(propEq(1, 'nullable', {}));
+
+//
+// optional
+//
+
+// happy path works as expected
+expectType<boolean>(propEq(1)('optional')(obj));
+expectType<boolean>(propEq(1, 'optional')(obj));
+expectType<boolean>(propEq(1, 'optional', obj));
+
+expectType<boolean>(propEq(undefined)('optional')(obj));
+expectType<boolean>(propEq(undefined, 'optional')(obj));
 expectType<boolean>(propEq(undefined, 'optional', obj));
-// unknownKey errors
-expectError(propEq('whatever', 'unknownKey', obj));
+
+// `null` produces error for `optional`. this is expected because typescript strictNullCheck `null !== undefined`
+expectError(propEq(null)('optional')(obj));
+expectError(propEq(null, 'optional')(obj));
+expectError(propEq(null, 'optional', obj));
+
+// rejects if typeof val not part of union type
+expectError(propEq(true)('optional')(obj));
+expectError(propEq(true, 'optional')(obj));
+expectError(propEq(true, 'optional', obj));
+
+// rejects if key is not on obj
+expectError(propEq(1)('optional')({} as Omit<Obj, 'optional'>));
+expectError(propEq(1, 'optional')({} as Omit<Obj, 'optional'>));
+expectError(propEq(1, 'optional', {} as Omit<Obj, 'optional'>));
+
+// rejects empty object literal literal
+expectError(propEq(1)('optional')({}));
+expectError(propEq(1, 'optional')({}));
+expectError(propEq(1, 'optional', {}));
+
+//
+// other non-happy paths
+//
+
+// rejects unknown key
+expectError(propEq(1)('whatever')(obj));
+expectError(propEq(1, 'whatever')(obj));
+expectError(propEq(1, 'whatever', obj));
+
+// rejects unknown key on emptyu object literal
+expectError(propEq(1)('whatever')({}));
+expectError(propEq(1, 'whatever')({}));
+expectError(propEq(1, 'whatever', {}));
--- types/propEq.d.ts
+++ types/propEq.d.ts
@@ -1,14 +1,12 @@
-import { WidenLiterals } from './util/tools';
-
 // propEq(val)
-export function propEq<T>(val: T): {
+export function propEq<const T>(val: T): {
   // propEq(val)(name)(obj)
-  <K extends PropertyKey>(name: K): <U extends Partial<Record<K, any>>>(obj: U) => T extends WidenLiterals<U[K]> ? boolean : never;
+  <K extends PropertyKey>(name: K): <U extends Partial<Record<K, any>>>(obj: Required<U> extends Record<K, any> ? T extends U[K] ? U : never : never) => boolean;
   // propEq(val)(name, obj)
   // type it this way for better error message for unknown keys
-  <K extends keyof U, U extends Partial<Record<PropertyKey, any>>>(name: K, obj: U): T extends WidenLiterals<U[K]> ? boolean : never;
+  <K extends keyof U, U extends Partial<Record<K, any>>>(name: K, obj: Required<U> extends Record<K, any> ? T extends U[K] ? U : never : never): boolean;
 };
 // propEq(val, name)(obj)
-export function propEq<T, K extends PropertyKey>(val: T, name: K): <U extends Partial<Record<PropertyKey, any>>>(obj: U) => T extends WidenLiterals<U[K]> ? boolean : never;
+export function propEq<const T, K extends PropertyKey>(val: T, name: K): <U extends Partial<Record<K, any>>>(obj: Required<U> extends Record<K, any> ? T extends U[K] ? U : never : never) => boolean;
 // propEq(val, name, obj)
 export function propEq<K extends keyof U, U>(val: U[K], name: K, obj: U): boolean;
