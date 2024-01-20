0559adef2e13ec530da23a5f9226fc96b3cb67fc
harris-miller <harrismillerconsulting@gmail.com>
2024-01-20 15:22:16 -0700

    better pick

--- test/pick.test.ts
+++ test/pick.test.ts
@@ -11,7 +11,7 @@ expectType<{ foo: number; }>(pick(['foo'])(obj));
 expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'])(obj));
 expectType<{ foo: number; bar: string; biz?: boolean }>(pick(['foo', 'bar', 'biz'])(obj));
 // as long as some of the array has valid keys, it will accept it, but will return `never` if there are any invalid keys
-expectType<never>(pick(['baz', 'bar', 'biz'])(obj));
+expectError(pick(['baz', 'bar', 'biz'])(obj));
 // if the array is only invalid keys, it will error
 expectError(pick(['baz'])(obj));
 // make sure typed array works
