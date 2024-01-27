29c9cf25c18b6ee9055748ddfbbf4b6a88e78da6
Alexandre Germain <alexandre.germain@craft.ai>
2024-01-27 01:04:59 +0100

    feat(zipObj): stricter types, add test

--- types/zipObj.d.ts
+++ types/zipObj.d.ts
@@ -1,3 +1,14 @@
-export function zipObj<K extends PropertyKey>(keys: readonly K[]): <T>(values: readonly T[]) => { [P in K]: T };
-export function zipObj<T, K extends PropertyKey>(keys: readonly K[], values: readonly T[]): { [P in K]: T };
+import * as _ from 'ts-toolbelt';
 
+export type _ZipObj<K extends readonly PropertyKey[], V extends {[key in keyof K]: unknown} | readonly unknown[]> =
+    number extends V['length'] ?
+      { [T in K[number]]: V[number] } :
+      number extends K['length'] ?
+        { [T in K[number]]: V[number] } : _.L.ZipObj<K, V>;
+
+export function zipObj<K extends readonly PropertyKey[]>(keys: _.F.Narrow<K>):
+<V extends {[key in keyof K]: unknown} | readonly unknown[]>(values: _.F.Narrow<V>) =>
+_ZipObj<K, V>;
+
+export function zipObj<K extends readonly PropertyKey[], V extends {[key in keyof K]: unknown} | readonly unknown[]>(keys: _.F.Narrow<K>, values: _.F.Narrow<V>):
+_ZipObj<K, V>;
