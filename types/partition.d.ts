87175495a6bc97f25a403c92342c294752c8bc01
Alexandre Germain <alexandre.germain@craft.ai>
2024-01-31 10:10:59 +0100

    chore: reorder declarations

--- types/partition.d.ts
+++ types/partition.d.ts
@@ -1,5 +1,5 @@
-export function partition<T, U extends T>(fn: (a: T) => a is U, list: readonly T[]): [U[], Exclude<T, U>[]];
-export function partition<T>(fn: (a: T) => boolean, list: readonly T[]): [T[], T[]];
-
 export function partition<T, U extends T>(fn: (a: T) => a is U): <L extends T = T>(list: readonly L[]) => [U[], Exclude<L, U>[]];
 export function partition<T>(fn: (a: T) => boolean): <L extends T = T>(list: readonly L[]) => [L[], L[]];
+
+export function partition<T, U extends T>(fn: (a: T) => a is U, list: readonly T[]): [U[], Exclude<T, U>[]];
+export function partition<T>(fn: (a: T) => boolean, list: readonly T[]): [T[], T[]];
