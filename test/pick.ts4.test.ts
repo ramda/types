704eb60709e5d7ca54e4cb076f3dfd04af7295ab
harris-miller <harrismillerconsulting@gmail.com>
2024-02-14 22:08:09 -0700

    tests

--- test/pick.ts4.test.ts
+++ test/pick.ts4.test.ts
@@ -2,25 +2,29 @@ import { expectType, expectError } from 'tsd';
 
 import { pick } from '../ts4/es';
 
-const obj = { foo: 1, bar: '2', biz: false };
+type Obj = { foo: number; bar: string; biz?: boolean };
+
+const obj = {} as Obj;
 
 expectType<{}>(pick([])(obj));
-expectType<Partial<typeof obj>>(pick(['foo'])(obj));
-expectType<Partial<typeof obj>>(pick(['foo', 'bar'])(obj));
-expectType<Partial<typeof obj>>(pick(['foo', 'bar', 'biz'])(obj));
-// this won't error because ts4 can't automatically try and deduce the values in the array
-expectType<Partial<typeof obj>>(pick(['baz', 'bar', 'biz'])(obj));
-// it can when the array is typed as a const
+expectType<{ foo: number; }>(pick(['foo'] as const)(obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'] as const)(obj));
+expectType<{ foo: number; bar: string; biz?: boolean }>(pick(['foo', 'bar', 'biz'] as const)(obj));
+// as long as some of the array has valid keys, it will accept it, but will return `never` if there are any invalid keys
 expectError(pick(['baz', 'bar', 'biz'] as const)(obj));
+// errors (with better message) with array of all unknown keys
+expectError(pick(['baz'] as const)(obj));
+// make sure typed array works
 expectType<typeof obj>(pick([] as (keyof typeof obj)[])(obj));
 
 expectType<{}>(pick([], obj));
-expectType<Partial<typeof obj>>(pick(['foo'], obj));
-expectType<Partial<typeof obj>>(pick(['foo', 'bar'], obj));
-expectType<Partial<typeof obj>>(pick(['foo', 'bar', 'biz'], obj));
-// binary function works as expect though because `names` is typed as `(keyof obj)[]`
+expectType<{ foo: number; }>(pick(['foo'], obj));
+expectType<{ foo: number; bar: string; }>(pick(['foo', 'bar'], obj));
+expectType<{ foo: number; bar: string; biz?: boolean }>(pick(['foo', 'bar', 'biz'], obj));
 expectError(pick(['baz', 'bar', 'biz'], obj));
+// make sure typed array works
 expectType<typeof obj>(pick([] as (keyof typeof obj)[], obj));
 
 // Record
-expectType<Record<string, number>>(pick(['foo', 'bar'], {} as Record<string, number>));
+expectType<Pick<Record<string, number>, 'foo' | 'bar'>>(pick(['foo', 'bar'] as const)({} as Record<string, number>));
+expectType<Pick<Record<string, number>, 'foo' | 'bar'>>(pick(['foo', 'bar'], {} as Record<string, number>));
