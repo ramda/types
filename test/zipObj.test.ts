4def9b56c00e2bf9fda37c14266e559b498948d1
Alexandre Germain <alexandre.germain@craft.ai>
2024-02-22 12:21:28 +0100

    chore: apply feedbacks (1 test failing)

--- test/zipObj.test.ts
+++ test/zipObj.test.ts
@@ -11,6 +11,7 @@ expectType<{bool: boolean, number: number}>(zipObj(['bool', 'number'], [true, 1]
 expectType<{[key: string]: boolean}>(zipObj(['a'] as string[], [true, false] as boolean[]));
 expectType<{a: boolean}>(zipObj(['a'], [true, false] as boolean[]));
 expectType<{[key: string]: boolean}>(zipObj(['a'] as string[], [true, false]));
+expectType<{[key: string]: boolean | number | string}>(zipObj(['a', 'b', 'c'] as string[], [true, 1, 'a']));
 expectType<{bool: true, number: 1}>(zipObj(['bool', 'number'], [true, 1, 'string']));
 expectType<{bool: true, number: 1, missing: undefined}>(zipObj(['bool', 'number', 'missing'], [true, 1]));
 
--- types/zipObj.d.ts
+++ types/zipObj.d.ts
@@ -1,10 +1,5 @@
 import * as _ from 'ts-toolbelt';
-
-export type _ZipObj<K extends readonly PropertyKey[], V extends {[key in keyof K]: unknown} | readonly unknown[]> =
-    number extends V['length'] ?
-      { [T in K[number]]: V[number] } :
-      number extends K['length'] ?
-        { [T in K[number]]: V[number] } : _.L.ZipObj<K, V>;
+import { _ZipObj } from './util/zipObj';
 
 export function zipObj<K extends readonly PropertyKey[]>(keys: _.F.Narrow<K>):
 <V extends {[key in keyof K]: unknown} | readonly unknown[]>(values: _.F.Narrow<V>) =>
