dd30f77388c9a5ba012a5cf000b80afe96324f50
harris-miller <harrismillerconsulting@gmail.com>
2024-04-02 01:24:50 -0600

    update a comment

--- types/tail.d.ts
+++ types/tail.d.ts
@@ -1,6 +1,5 @@
 // string
 export function tail(list: string): string;
-// non-empty tuples and array
 // `infer Tail` only works on types like `readonly [1, '2', 3]` where you will get back `['2', 3]`
 // else, if the type is `string[]`, you'll get back `string[]`
 export function tail<T extends readonly [...any]>(list: T): T extends readonly [any, ...infer Tail] ? Tail : T;
