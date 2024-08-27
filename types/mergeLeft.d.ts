980515c5bdcc18efd1700cea20bb4d4b2db8b565
harris-miller <harrismillerconsulting@gmail.com>
2024-08-26 22:57:42 -0600

    might as well apply to mergeLeft and mergeRight as well

--- types/mergeLeft.d.ts
+++ types/mergeLeft.d.ts
@@ -1,5 +1,7 @@
 import * as _ from 'ts-toolbelt';
 
 // Note: ramda `mergeLeft` uses `Object.assign` in code, so we need to use `O.Assign` here, and not `O.Merge`
+export function mergeLeft<U extends object>(l: U): (r: U) => U;
 export function mergeLeft<L extends object>(l: L): <R extends object>(r: R) => _.O.Assign<R, [L], 'flat'>;
+export function mergeLeft<U extends object>(l: U, r: U): U;
 export function mergeLeft<L extends object, R extends object>(l: L, r: R): _.O.Assign<R, [L], 'flat'>;
--- types/mergeRight.d.ts
+++ types/mergeRight.d.ts
@@ -1,5 +1,7 @@
 import * as _ from 'ts-toolbelt';
 
 // Note: ramda `mergeLeft` uses `Object.assign` in code, so we need to use `O.Assign` here, and not `O.Merge`
+export function mergeRight<U extends object>(l: U): (r: U) => U;
 export function mergeRight<L extends object>(l: L): <R extends object>(r: R) => _.O.Assign<L, [R], 'flat'>;
+export function mergeRight<U extends object>(l: U, r: U): U;
 export function mergeRight<L extends object, R extends object>(l: L, r: R): _.O.Assign<L, [R], 'flat'>;
