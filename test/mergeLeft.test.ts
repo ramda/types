e135d71b3fa0b7126651ca77a76d8985c8a6a495
harris-miller <harrismillerconsulting@gmail.com>
2024-08-26 23:01:47 -0600

    add tests for mergeLeft and mergeRight

--- test/mergeLeft.test.ts
+++ test/mergeLeft.test.ts
@@ -7,3 +7,17 @@ const foo2 = { foo: 2 };
 
 expectType<{ foo: string; bar: string; }>(mergeLeft(foo, bar));
 expectType<{ foo: string; }>(mergeLeft(foo, foo2));
+
+type TestType = {
+  foo: string;
+  bar: string;
+};
+
+const obj1 = {} as TestType;
+const obj2 = {} as TestType;
+
+expectType<TestType>(mergeLeft(obj1, obj2));
+expectType<TestType>(mergeLeft(obj2, obj1));
+
+expectType<TestType>(mergeLeft(obj1, { foo: '', bar: ''}));
+expectType<TestType>(mergeLeft({ foo: '', bar: ''}, obj2));
--- test/mergeRight.test.ts
+++ test/mergeRight.test.ts
@@ -7,3 +7,17 @@ const foo2 = { foo: 2 };
 
 expectType<{ foo: string; bar: string; }>(mergeRight(foo, bar));
 expectType<{ foo: number; }>(mergeRight(foo, foo2));
+
+type TestType = {
+  foo: string;
+  bar: string;
+};
+
+const obj1 = {} as TestType;
+const obj2 = {} as TestType;
+
+expectType<TestType>(mergeRight(obj1, obj2));
+expectType<TestType>(mergeRight(obj2, obj1));
+
+expectType<TestType>(mergeRight(obj1, { foo: '', bar: ''}));
+expectType<TestType>(mergeRight({ foo: '', bar: ''}, obj2));
