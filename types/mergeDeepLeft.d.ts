fde4e60f8ee5ae5eb86275df65bf6dcfb1c5ae72
harris-miller <harrismillerconsulting@gmail.com>
2024-08-26 22:56:38 -0600

    optimize mergeDeep funcs to return the type given if l and r are the same type

--- types/mergeDeepLeft.d.ts
+++ types/mergeDeepLeft.d.ts
@@ -1,4 +1,6 @@
 import * as _ from 'ts-toolbelt';
 
+export function mergeDeepLeft<U extends object>(l: U): (r: U) => U;
 export function mergeDeepLeft<L extends object>(l: L): <R extends object>(r: R) => _.O.Assign<R, [L], 'deep'>;
+export function mergeDeepLeft<U extends object>(l: U, r: U): U;
 export function mergeDeepLeft<L extends object, R extends object>(l: L, r: R): _.O.Assign<R, [L], 'deep'>;
--- types/mergeDeepRight.d.ts
+++ types/mergeDeepRight.d.ts
@@ -1,4 +1,6 @@
 import * as _ from 'ts-toolbelt';
 
+export function mergeDeepRight<U extends object>(l: U): (r: U) => U;
 export function mergeDeepRight<L extends object>(l: L): <R extends object>(r: R) => _.O.Assign<L, [R], 'deep'>;
+export function mergeDeepRight<U extends object>(l: U, r: U): U;
 export function mergeDeepRight<L extends object, R extends object>(l: L, r: R): _.O.Assign<L, [R], 'deep'>;
