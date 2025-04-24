ef2642cc113cc6cc6d039d33bf8c4fedce9b6b19
harris-miller <harrismillerconsulting@gmail.com>
2025-04-23 18:41:29 -0600

    curry them

--- types/mapKeys.d.ts
+++ types/mapKeys.d.ts
@@ -1,1 +1,8 @@
-declare function mapKeys<T>(fn: (key: string) => string, obj: Record<string, T>): Record<string, T>;
+import { Placeholder } from './util/tools';
+
+// mapKeys(fn)(obj)
+export function mapKeys(fn: (key: string) => string): <T>(obj: Record<string, T>) => Record<string, T>;
+// mapKeys(__, obj)(fn)
+export function mapKeys<T>(__: Placeholder, obj: Record<string, T>): (fn: (key: string) => string) => Record<string, T>;
+// mapKeys(fn, obj)
+export function mapKeys<T>(fn: (key: string) => string, obj: Record<string, T>): Record<string, T>;
--- types/rebuild.d.ts
+++ types/rebuild.d.ts
@@ -1,1 +1,8 @@
+import { Placeholder } from './util/tools';
+
+// rebuild(fn)(obj)
+export function rebuild<A, B>(fn: (kvp: [string, A]) => [string, B][]): (obj: Record<string, A>) => Record<string, B>;
+// rebuild(__, obj)(fn)
+export function rebuild<A>(__: Placeholder, obj: Record<string, A>): <B>(fn: (kvp: [string, A]) => [string, B][]) => Record<string, B>;
+// rebuild(fn, obj)
 export function rebuild<A, B>(fn: (kvp: [string, A]) => [string, B][], obj: Record<string, A>): Record<string, B>;
