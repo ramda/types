dfde053191a1eb9b8d4fe3f6f6d98fc2c8dc83d5
harris-miller <harrismillerconsulting@gmail.com>
2025-07-02 21:39:38 -0600

    simplify rebuild types

--- types/rebuild.d.ts
+++ types/rebuild.d.ts
@@ -1,8 +1,8 @@
 import { Placeholder } from './util/tools';
 
 // rebuild(fn)(obj)
-export function rebuild<Obj extends object, OutValue>(fn: (kvp: [keyof Obj, Obj[keyof Obj]]) => [string, OutValue][]): (obj: Obj) => Record<string, OutValue>;
+export function rebuild<T, V>(fn: (kvp: [keyof T, T[keyof T]]) => [string, V][]): (obj: T) => Record<string, V>;
 // rebuild(__, obj)(fn)
-export function rebuild<Obj extends object>(__: Placeholder, obj: Obj): <OutValue>(fn: (kvp: [keyof Obj, Obj[keyof Obj]]) => [string, OutValue][]) => Record<string, OutValue>;
+export function rebuild<T>(__: Placeholder, obj: T): <V>(fn: (kvp: [keyof T, T[keyof T]]) => [string, V][]) => Record<string, V>;
 // rebuild(fn, obj)
-export function rebuild<Obj extends object, OutValue>(fn: (kvp: [keyof Obj, Obj[keyof Obj]]) => [string, OutValue][], obj: Obj): Record<string, OutValue>;
+export function rebuild<T, V>(fn: (kvp: [keyof T, T[keyof T]]) => [string, V][], obj: T): Record<string, V>;
